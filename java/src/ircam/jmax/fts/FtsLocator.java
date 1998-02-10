67fb94abc387aeeeaa40e7a76a0edc1dbf5eb96a
dececco <dececco>
1998-02-10 12:32:38 +0000

    *** empty log message ***

---
+++ java/src/ircam/jmax/fts/FtsLocator.java
@@ -0,0 +1,15 @@
+package ircam.jmax.fts;
+
+/** An abstract locator for data inside FTS.
+ *To be really implemented 
+ */
+
+public class FtsLocator
+{
+  String address;
+
+  public FtsLocator(String address)
+  {
+    this.address = address;
+  }
+}
