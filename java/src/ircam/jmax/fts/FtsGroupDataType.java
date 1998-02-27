b28ca4fb473991c745cd91461d846afb0f310f77
maggi <maggi>
1998-02-27 15:31:14 +0000

    *** empty log message ***

---
+++ java/src/ircam/jmax/fts/FtsGroupDataType.java
@@ -0,0 +1,48 @@
+package ircam.jmax.fts;
+
+import java.util.*;
+
+import ircam.jmax.*;
+import ircam.jmax.mda.*;
+
+/** Class defining the fts group type (named patch);
+ * Added by enzo.
+ */
+
+public class FtsGroupDataType extends MaxDataType
+{
+  public FtsGroupDataType()
+  {
+    super("group");
+  }
+
+  /** Build an empty instance of FtsGroupData, i.e. an instance
+   * corresponding to an empty, new, patcher
+   */
+
+  public MaxData newInstance()
+  {
+    // Build a new FtsObject, a patcher 0 in 0 out
+    Vector aVector = new Vector();
+    /*FtsObject patcher;
+      
+      patcher = new FtsPatcherObject(MaxApplication.getFtsServer().getRootObject());
+      
+      // Put some geometrical property for the window, so we can see it.
+      
+      patcher.put("win.pos.x", 100);
+      patcher.put("win.pos.y", 100);
+      patcher.put("win.size.w", 500);
+      patcher.put("win.size.h", 400);
+      */
+    FtsGroupData obj = new FtsGroupData();
+    
+    obj.setGroup(aVector);
+    
+    return obj;
+  }
+}
+
+
+
+
