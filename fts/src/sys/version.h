b35a9cfeb295b57ae4161edec9cc0c80e6004dfc
dechelle <dechelle>
1998-03-18 15:39:32 +0000

    Added setpriority code in gnuhw.c Changed version number

--- fts/src/sys/version.h
+++ fts/src/sys/version.h
@@ -1,8 +1,8 @@
 #define FTS_MAJOR_RELEASE  2
 #define FTS_MINOR_RELEASE  1
 #define FTS_PATCH_LEVEL    1
-#define FTS_RELEASE_QUALIFICATION "beta"
-#define FTS_RELEASE_STRING "FTS release 2.1 patch level 1 beta \n"
+#define FTS_RELEASE_QUALIFICATION "beta (Forum CD - April 1998)"
+#define FTS_RELEASE_STRING "FTS release 2.1 patch level 1 beta (Forum CD - April 1998)\n"
 #define FTS_COMPILATION_DATE   __DATE__
 #define FTS_COMPILATION_TIME   __TIME__
 
