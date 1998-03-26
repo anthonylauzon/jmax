38ef7917deff82bfc346902d78d8d3a5f640cce9
dececco <dececco>
1998-03-26 09:24:00 +0000

    Missinfg files.

---
+++ java/src/ircam/jmax/fts/FtsAtomList.java
@@ -0,0 +1,46 @@
+package ircam.jmax.fts;
+
+/**
+ * This class represent an Atom list 
+ * Atom lists cannot  be updated/changed partially, for the moment.
+ * A string representation is supported for atom lists
+ */
+
+class FtsAtomList
+{
+  FtsAtomVector()
+  {
+  }
+
+  /** Get the list in text form */
+
+  public String getValuesAsText()
+  {
+  }
+
+  /** Set the list as text */
+  
+  public void setValuesAsText(String value)
+  {
+  }
+
+  /** Get the list   */
+
+  public Vector values()
+  {
+  }
+
+  /** Get the whole content  of the vector from FST */
+
+  public void forceUpdate()
+  {
+  }
+
+  /** Declare that all the  values in the vector has been changed
+      and should be sent to FTS
+      */
+
+  public void changed()
+  {
+  }
+}
---
+++ java/src/ircam/jmax/fts/FtsTableObject.java
@@ -0,0 +1,114 @@
+package ircam.jmax.fts;
+
+import java.util.*;
+import java.io.*;
+
+import ircam.jmax.*;
+import ircam.jmax.mda.*;
+
+/**
+ * Class implementing the proxy of an FTS table object.
+ */
+
+public class FtsTableObject extends FtsObject implements FtsIntegerVectorObject, FtsDataObject
+{
+  FtsIntegerVector vector = null;
+  int vectorSize = 128;
+
+  /*****************************************************************************/
+  /*                                                                           */
+  /*                               CONSTRUCTORS                                */
+  /*                                                                           */
+  /*****************************************************************************/
+
+  /**
+   * Create a FtsObject object;
+   */
+
+  FtsTableObject(FtsContainerObject parent, String className, String description)
+  {
+    super(parent, className, description);
+
+    FtsServer.getServer().newObject(parent, this, description);
+
+    //Then, look for the size in the argument
+
+    Vector args;
+
+    args = new Vector();
+    
+    FtsParse.parseObjectArguments(description, args);
+
+    if ((args.size() >= 3) && (args.elementAt(0) instanceof Integer))
+      vectorSize = ((Integer) args.elementAt(0)).intValue();
+
+    if (parent.isOpen())
+      FtsServer.getServer().syncToFts();
+  }
+
+  /** Send the whole vector content to fts */
+
+  public void saveVectorToFts()
+  {
+    this.vector.changed();
+    FtsServer.getServer().syncToFts();
+  }
+
+  /** Load the whole vector content from fts */
+
+  public void loadVectorFromFts()
+  {
+    this.vector.forceUpdate();
+    FtsServer.getServer().syncToFts();
+  }
+
+  /** Bind this obejct to a vector; this means
+    handle from now on all the bidirectional updates
+    and similar things by redirecting them to the
+    vector
+    */
+
+  public void bindVector(FtsIntegerVector vector)
+  {
+    this.vector = vector;
+    vector.setObject(this);
+    vector.setSize(vectorSize);
+  }
+
+  /** unbind it */
+
+  public void unbindVector(FtsIntegerVector vector)
+  {
+    // We may have a pending save/update
+    vector.setObject(null);
+    FtsServer.getServer().syncToFts();
+  }
+
+  /** Tell MDA that this FTS object support a integerVector data object */
+
+  public MaxDataType getObjectDataType()
+  {
+    return MaxDataType.getTypeByName("integerVector");
+  }
+
+
+  // Save the object *and* the table; it must be
+  // sure to have the complete table, so it ask it 
+  // to fts; this is orribly slow, of course
+  // TO be done 
+
+  public void saveAsTcl(PrintWriter writer)
+  {
+    // Save as "object ..."
+
+    writer.print("object {" + description + "}");
+
+    savePropertiesAsTcl(writer);
+  }
+}
+
+
+
+
+
+
