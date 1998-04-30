5e75bcb27fa555e6b2139543bf4cbadeb159bae9
dechelle <dechelle>
1998-04-30 12:26:57 +0000

    audio.tcl is a mess !

--- config/audio.tcl
+++ config/audio.tcl
@@ -64,38 +64,41 @@ if {$jmaxArch == "irix6.2" || $jmaxArch == "irix5.3" || $jmaxArch == "mips1"} {
     #ucs open device in~ multi as SgiALIn ALdevice ADATIn channels 8 fifo_size $jmaxAudioBuffer
     #ucs open device out~ multi as SgiALOut ALdevice ADATOut channels 8 fifo_size $jmaxAudioBuffer
 
-    #puts "    stereo: 2/2"
-    #ucs open device in~ stereo as SgiALIn ALdevice AESIn channels 2 fifo_size $jmaxAudioBuffer
-    #ucs open device out~ stereo as SgiALOut ALdevice AESOut channels 2 fifo_size $jmaxAudioBuffer
+#puts "    stereo: 2/2"
+#ucs open device in~ stereo as SgiALIn ALdevice AESIn channels 2 fifo_size $jmaxAudioBuffer
+#ucs open device out~ stereo as SgiALOut ALdevice AESOut channels 2 fifo_size $jmaxAudioBuffer
 
-    #ucs default in~ multi
-    #ucs default out~ multi
+#ucs default in~ multi
+#ucs default out~ multi
 
     # two ADAT cards
 
-    puts "    multi1: 8/8 (default)"
-    ucs open device in~ multi1 as SgiALIn ALdevice RAD1.ADATIn channels 8 fifo_size $jmaxAudioBuffer
-    ucs open device out~ multi1 as SgiALOut ALdevice RAD1.ADATOut channels 8 fifo_size $jmaxAudioBuffer
+#     puts "    multi1: 8/8 (default)"
+#     ucs open device in~ multi1 as SgiALIn ALdevice RAD1.ADATIn channels 8 fifo_size $jmaxAudioBuffer
+#     ucs open device out~ multi1 as SgiALOut ALdevice RAD1.ADATOut channels 8 fifo_size $jmaxAudioBuffer
 
-    #puts "    stereo1: 2/2"
-    #ucs open device in~ stereo1 as SgiALIn ALdevice RAD1.AESIn channels 2 fifo_size $jmaxAudioBuffer
-    #ucs open device out~ stereo1 as SgiALOut ALdevice RAD1.AESOut channels 2 fifo_size $jmaxAudioBuffer
+#    puts "    stereo1: 2/2"
+#    ucs open device in~ stereo1 as SgiALIn ALdevice RAD1.AESIn channels 2 fifo_size $jmaxAudioBuffer
+#    ucs open device out~ stereo1 as SgiALOut ALdevice RAD1.AESOut channels 2 fifo_size $jmaxAudioBuffer
 
     #puts "    multi2: 8/8"
     #ucs open device in~ multi2 as SgiALIn ALdevice RAD2.ADATIn channels 8 fifo_size $jmaxAudioBuffer
     #ucs open device out~ multi2 as SgiALOut ALdevice RAD2.ADATOut channels 8 fifo_size $jmaxAudioBuffer
 
-    #puts "    stereo2: 2/2"
+#    puts "    stereo2: 0/2"
     #ucs open device in~ stereo2 as SgiALIn ALdevice RAD2.AESIn channels 2 fifo_size $jmaxAudioBuffer
-    #ucs open device out~ stereo2 as SgiALOut ALdevice RAD2.AESOut channels 2 fifo_size $jmaxAudioBuffer
+#    ucs open device out~ stereo2 as SgiALOut ALdevice RAD2.AESOut channels 2 fifo_size $jmaxAudioBuffer
 
-    ucs default in~ multi1
-    ucs default out~ multi1
+#     ucs default in~ multi1
+#     ucs default out~ multi1
+#    ucs default in~ stereo1
+#    ucs default out~ stereo1
 
 } elseif {$jmaxArch == "linuxpc"} {
 
     puts "    default: 2/2"
-    ucs open device out~ default as oss_dac
+#    ucs open device out~ default as oss_dac
+    ucs open device out~ default as oss_dac fragment_size 4096 max_fragments 8
     #ucs open device in~ default as oss_adc
    
     ucs default out~ default 
