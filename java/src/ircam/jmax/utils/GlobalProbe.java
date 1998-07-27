d362c859e42a99ec0dda8ccb1d92e48074918c1f
dechelle <dechelle>
1998-07-27 10:48:36 +0000

    Changed completely Probe and GlobalProbe.

--- java/src/ircam/jmax/utils/GlobalProbe.java
+++ java/src/ircam/jmax/utils/GlobalProbe.java
@@ -2,21 +2,52 @@
 package ircam.jmax.utils;
 
 public class GlobalProbe {
-  static Probe itsProbe;
 
-  public static void makeGlobalProbe() {
-    itsProbe = new Probe("global");
-    itsProbe.setFile( "global");
+  static Probe probe;
+
+  static
+  {
+    probe = new Probe( "global");
   }
 
-  public static void report() {
-    if (itsProbe != null) 
-      itsProbe.report();
+  // @deprecated
+  public static void makeGlobalProbe()
+  {
   }
 
-  public static void mark(String theMark) {
-    if (itsProbe != null) 
-      itsProbe.mark(theMark);
+  public static void setSynchronous( boolean synchronous)
+  {
+    probe.setSynchronous( synchronous);
+  }
+
+  public static void report() 
+  {
+    probe.report();
+  }
+
+  public static void setOutputToFile()
+  {
+    probe.setOutputToFile();
+  }
+
+  public static void enterMethod( Object object, String methodName, String extraMsg)
+  {
+    probe.enterMethod( object, methodName, extraMsg);
+  }
+
+  public static void enterMethod( Object object, String methodName)
+  {
+    probe.enterMethod( object, methodName);
+  }
+
+  public static void exitMethod()
+  {
+    probe.exitMethod();
+  }
+
+  public static void mark(String markName) 
+  {
+    probe.mark( markName);
   }
 
   // @deprecated
--- java/src/ircam/jmax/utils/Probe.java
+++ java/src/ircam/jmax/utils/Probe.java
@@ -3,6 +3,7 @@ package ircam.jmax.utils;
 import java.lang.*;
 import java.io.*;
 import java.util.*;
+import java.text.*;
 
 public class Probe {
 
@@ -24,32 +25,55 @@ public class Probe {
   public Probe( String name, int maxEvents) 
   {
     this.name = name;
-    this.maxEvents = maxEvents;
+
+    synchronous = false;
 
     prevTime = 0;
-    reportCount = 0;
-    times = new long[maxEvents];
-    marks = new String[maxEvents]; 
-    count = 0;
 
-    printlnAdapter = new SystemOutPrintlnAdapter();
+    count = 0;
+    reportCount = 0;
+    globalCount = 0;
+
+    currentDepth = 0;
+
+    times = new long[maxEvents];
+    depths = new int[maxEvents];
+    marks = new String[maxEvents]; 
+
+    printlnAdapter = null;
+
+    rj10 = new RightJustify( 10, ' ', true);
+    rj4 = new RightJustify( 4, ' ', false);
+  }
+
+  public void setSynchronous( boolean synchronous)
+  {
+    this.synchronous = synchronous;
+  }
+
+  public boolean getSynchronous()
+  {
+    return synchronous;
   }
 
   private interface Println {
     public void println( String s);
+    public void flush();
   }
 
   private class FileOutPrintlnAdapter implements Println {
     FileOutPrintlnAdapter( PrintWriter printOut) { this.printOut = printOut; }
     public void println( String s) { printOut.println( s); }
+    public void flush() { printOut.flush(); }
     private PrintWriter printOut;
   }
 
-  private class SystemOutPrintlnAdapter implements Println {
-    public void println( String s) { System.out.println( s); }
+  private class SystemErrPrintlnAdapter implements Println {
+    public void println( String s) { System.err.println( s); }
+    public void flush() { System.err.flush(); }
   }
 
-  public void setFile( String fileName)
+  public void setOutputToFile()
   {
     try
       {
@@ -64,40 +88,146 @@ public class Probe {
       }
   }
 
-  public void mark(String markName) 
+  public void enterMethod( Object object, String methodName, String extraMsg)
   {
-    if (count >= maxEvents)
+    mark( object.getClass().getName() + "." + methodName + "() " + extraMsg + " {");
+    currentDepth++;
+  }
+
+  public void enterMethod( Object object, String methodName)
+  {
+    mark( object.getClass().getName() + "." + methodName + "() {");
+    currentDepth++;
+  }
+
+  public void exitMethod()
+  {
+    currentDepth--;
+    mark( "}");
+  }
+
+  protected void mark(String markName) 
+  {
+    int nEmpty = reportCount - count - 1;
+
+    if (nEmpty < 0)
+      nEmpty += times.length;
+      
+    if ( nEmpty == 0)
       {
 	report();
-	count = 0;
       }
 
     times[count] = System.currentTimeMillis();
+    depths[count] = currentDepth;
     marks[count] = markName;
+
     count++;
+    if (count >= times.length)
+      count = 0;
+
+    if (synchronous)
+      report();
   }  
+
+  private class RightJustify {
+    protected RightJustify( int fieldLength, char pad, boolean signed)
+    {
+      numberFormat = NumberFormat.getInstance( Locale.US);
+
+      numberFormat.setMaximumFractionDigits(6);
+      numberFormat.setMinimumFractionDigits(0);
+      numberFormat.setGroupingUsed(false);
+
+      fieldPositionInteger = new FieldPosition( NumberFormat.INTEGER_FIELD);
+
+      this.fieldLength = fieldLength;
+      this.pad = pad;
+      this.signed = signed;
+    }
+
+    protected String format( long n)
+    {
+      StringBuffer buffer = new StringBuffer( 64);
+      int padLength;
+
+      numberFormat.format( n, buffer, fieldPositionInteger);
+
+      padLength = fieldLength - fieldPositionInteger.getEndIndex();
+
+      if (signed)
+	{
+	  if (n >= 0)
+	    {
+	      buffer.insert( 0, '+');
+	      padLength--;
+	    }
+	}
+
+      for ( int j = 0; j < padLength; j++)
+	buffer.insert( 0, pad);
+
+      return buffer.toString();
+    }
+
+    protected int fieldLength;
+    protected char pad;
+    protected boolean signed;
+    protected NumberFormat numberFormat;
+    protected FieldPosition fieldPositionInteger;
+  }
 
   public void report()
   {
-    for ( int i = 0; i < count; i++)
+    if (printlnAdapter == null)
+      printlnAdapter = new SystemErrPrintlnAdapter();
+
+    if (reportCount == 0)
+      prevTime = times[reportCount];
+
+    while (reportCount != count)
       {
-	long delta = times[i] - prevTime;
-	printlnAdapter.println( name + "[" + reportCount + "] mark='" + marks[i] + "' delta=" + delta + "ms " );
+	long delta = times[reportCount] - prevTime;
+
+	StringBuffer sb = new StringBuffer(128);
+
+	for( int i = 0; i < depths[reportCount]; i++)
+	  sb.append( "    ");
+
+	sb.append( "|- ").append( marks[reportCount]);
+
+	printlnAdapter.println( name + "[ " + rj4.format(reportCount) + " ] " + rj10.format(delta) + " " + sb);
+
+	prevTime = times[reportCount];
+
+	globalCount++;
 
 	reportCount++;
-	prevTime = times[i];
+	if ( reportCount >= times.length)
+	  reportCount = 0;
       }
   }
 
-  private static final int DEFAULT_MAX_EVENTS = 1024; 
   private String name;
-  private int maxEvents;
 
-  private int reportCount;
+  boolean synchronous;
+
   private long prevTime;
 
   private int count;
+  private int reportCount;
+  private int globalCount;
+
+  private int currentDepth;
+
   private long times[];
+  private int depths[];
   private String marks[];
+
   private Println printlnAdapter;
+
+  RightJustify rj10;
+  RightJustify rj4;
+
+  private static final int DEFAULT_MAX_EVENTS = 65536; 
 }
