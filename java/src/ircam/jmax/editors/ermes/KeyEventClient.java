3bc1c2e29f4d658b76052e17e64cd432d16315d2
dechelle <dechelle>
1998-07-28 09:10:23 +0000

    *** empty log message ***

---
+++ java/src/ircam/jmax/editors/ermes/KeyEventClient.java
@@ -0,0 +1,12 @@
+package ircam.jmax.editors.ermes;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public interface KeyEventClient extends KeyListener {
+  public void keyInputGained();
+  public void keyInputLost();
+}
+
+  
