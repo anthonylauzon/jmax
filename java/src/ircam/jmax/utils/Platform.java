933c0607a0a373dd4f1d49932a08262904424368
dechelle <dechelle>
1998-09-21 11:43:53 +0000

    Modified Platform.java to have arrow keys in Linux. Probably Platform.java is to be moved to the bin.

--- java/src/ircam/jmax/utils/Platform.java
+++ java/src/ircam/jmax/utils/Platform.java
@@ -17,6 +17,7 @@
 package ircam.jmax.utils;
 
 import java.awt.*;
+import java.awt.event.*;
 
 
 //=====================================================
@@ -25,153 +26,150 @@ import java.awt.*;
 
 public class Platform
 {
-   static public String NEWLINE     = null;
-   static public char   NEWLINECHAR = 0;
-   static public int    DELETE_KEY  = 0;
-   static public int    BACKSPACE_KEY  = 0;	//to check in all the platforms
-   static public int    ENTER_KEY   = 0;
-   static public int    RETURN_KEY  = 0;
-   static public int    FONT_SIZE   = 0;
-   static public String FONT_NAME   = null;
-   static public int    LEFT_KEY    = 1006;//??
-   static public int    RIGHT_KEY   = 1007;//??
-   static public int    UP_KEY      = 1004;//??
-   static public int    DOWN_KEY    = 1005;//??
+  static public String NEWLINE     = null;
+  static public char   NEWLINECHAR = 0;
+  static public int    DELETE_KEY  = 0;
+  static public int    BACKSPACE_KEY  = 0;	//to check in all the platforms
+  static public int    ENTER_KEY   = 0;
+  static public int    RETURN_KEY  = 0;
+  static public int    FONT_SIZE   = 0;
+  static public String FONT_NAME   = null;
+  static public int    LEFT_KEY    = KeyEvent.VK_LEFT;
+  static public int    RIGHT_KEY   = KeyEvent.VK_RIGHT;
+  static public int    UP_KEY      = KeyEvent.VK_UP;
+  static public int    DOWN_KEY    = KeyEvent.VK_DOWN;
 
-   static public String  SYSTEM     = null;
-   static public boolean INAPPLET   = false;
-
-
-
-//============================
-//   * mac: okay
-//
-//   * sgi:
-//       return = ?
-//       newlinechar = ?
-//   
-//   * dec-alpha ?
-//   * windows ?
-//   * linux ?
-//   
-//============================
+  static public String  SYSTEM     = null;
+  static public boolean INAPPLET   = false;
 
 
 
-   //------------------------------------------
-   // setValues
-   //------------------------------------------
+  //------------------------------------------
+  // setValues
+  //------------------------------------------
    
-   static public void setValues()
-   { 
-      
-      if (!INAPPLET){
-         SYSTEM =  (String)(System.getProperties().getProperty("os.name"));
-      }
-      else if (SYSTEM == null){
-         SYSTEM = "unknown";
-      }
-      if (SYSTEM.equalsIgnoreCase("macos")){
-         setMacValues();
-      }
-      else if (SYSTEM.equals("Irix")) // SGI
+  static public void setValues()
+  { 
+    if (!INAPPLET)
       {
-         setIrixValues();
+	SYSTEM =  (String)(System.getProperties().getProperty("os.name"));
       }
-      else if (SYSTEM.equals("DEC")){
-         setIrixValues();
-          }  
-      else if (SYSTEM.equals("windows")){
-         setWindowsValues();
+    else if (SYSTEM == null)
+      {
+	SYSTEM = "unknown";
+      }
+
+    if (SYSTEM.equalsIgnoreCase("macos"))
+      {
+	setMacValues();
+      }
+    else if (SYSTEM.equals("Irix"))
+      {
+	setIrixValues();
+      }
+    else if (SYSTEM.equals("DEC"))
+      {
+	setIrixValues();
       }  
-      else if (SYSTEM.equals("Linux")){
-         	setLinuxValues();
+    else if (SYSTEM.equals("windows"))
+      {
+	setWindowsValues();
       }  
-      else{
+    else if (SYSTEM.equals("Linux"))
+      {
+	setLinuxValues();
+      }  
+    else
+      {
 	setDefaultValues();
       }
-   }
+  }
 
-   //------------------------------------------
-   // setMacValues
-   //------------------------------------------
+  //------------------------------------------
+  // setMacValues
+  //------------------------------------------
    
-        static protected void setMacValues(){
-         	NEWLINE     = "\n";
-        	NEWLINECHAR = '\n';
-         	DELETE_KEY  = 8;
-        	BACKSPACE_KEY = 127;
-        	ENTER_KEY   = 3;
-         	RETURN_KEY  = 10;
-        	FONT_SIZE   = 9;
-         	FONT_NAME   = new String("monaco");
-        }
+  static protected void setMacValues()
+  {
+    NEWLINE     = "\n";
+    NEWLINECHAR = '\n';
+    DELETE_KEY  = 8;
+    BACKSPACE_KEY = 127;
+    ENTER_KEY   = 3;
+    RETURN_KEY  = 10;
+    FONT_SIZE   = 9;
+    FONT_NAME   = new String("monaco");
+  }
 
-   //------------------------------------------
-   // setIrixValues
-   //------------------------------------------
+  //------------------------------------------
+  // setIrixValues
+  //------------------------------------------
    
-        static protected void setIrixValues(){
-         	NEWLINE     = "\n";
-         	NEWLINECHAR = '\n';
-        	DELETE_KEY  = 127;
-         	BACKSPACE_KEY = 8;
-		LEFT_KEY    = 37;
-		UP_KEY      = 38;
-		RIGHT_KEY   = 39;
-		DOWN_KEY    = 40;
-         	ENTER_KEY   = 10;
-         	RETURN_KEY  = 10;
-         	FONT_SIZE   = 10;
-         	FONT_NAME   = new String("SansSerif");
-        }
+  static protected void setIrixValues()
+  {
+    NEWLINE     = "\n";
+    NEWLINECHAR = '\n';
+    DELETE_KEY  = 127;
+    BACKSPACE_KEY = 8;
+    LEFT_KEY    = 37;
+    UP_KEY      = 38;
+    RIGHT_KEY   = 39;
+    DOWN_KEY    = 40;
+    ENTER_KEY   = 10;
+    RETURN_KEY  = 10;
+    FONT_SIZE   = 10;
+    FONT_NAME   = new String("SansSerif");
+  }
 
-   //------------------------------------------
-   // setDECValues
-   //------------------------------------------
+  //------------------------------------------
+  // setDECValues
+  //------------------------------------------
    
-        static protected void setDECValues(){
-        	NEWLINE     = "\n";
-        	NEWLINECHAR = '\n';
-         	DELETE_KEY  = 8;
-         	ENTER_KEY   = 10;
-         	RETURN_KEY  = 10;
-         	FONT_SIZE   = 18;
-        	FONT_NAME   = new String("monaco");
-        }
+  static protected void setDECValues()
+  {
+    NEWLINE     = "\n";
+    NEWLINECHAR = '\n';
+    DELETE_KEY  = 8;
+    ENTER_KEY   = 10;
+    RETURN_KEY  = 10;
+    FONT_SIZE   = 18;
+    FONT_NAME   = new String("monaco");
+  }
 
-   //------------------------------------------
-   // setWindowsValues
-   //------------------------------------------
+  //------------------------------------------
+  // setWindowsValues
+  //------------------------------------------
    
-        static protected void setWindowsValues() {
-         	setDefaultValues();
-        }
+  static protected void setWindowsValues() 
+  {
+    setDefaultValues();
+  }
 
-   //------------------------------------------
-   // setLinuxValues
-   //------------------------------------------
+  //------------------------------------------
+  // setLinuxValues
+  //------------------------------------------
    
-        static protected void setLinuxValues(){
-	  setDefaultValues();
-        }
+  static protected void setLinuxValues()
+  {
+    setDefaultValues();
+  }
         
-   //------------------------------------------
-   // setDefaultValues
-   //------------------------------------------
+  //------------------------------------------
+  // setDefaultValues
+  //------------------------------------------
    
-        static protected void setDefaultValues(){
-         	NEWLINE     = "\n";
-         	NEWLINECHAR = '\n';
-        	 DELETE_KEY  = 8;
-        	 BACKSPACE_KEY = 127;
-         	ENTER_KEY   = 10;
-        	 RETURN_KEY  = 10;
-		 FONT_NAME = Toolkit.getDefaultToolkit().getFontList()[1];
-        	 FONT_SIZE   = 10;
+  static protected void setDefaultValues()
+  {
+    NEWLINE     = "\n";
+    NEWLINECHAR = '\n';
+    DELETE_KEY  = 8;
+    BACKSPACE_KEY = 127;
+    ENTER_KEY   = 10;
+    RETURN_KEY  = 10;
+    FONT_NAME = Toolkit.getDefaultToolkit().getFontList()[1];
+    FONT_SIZE   = 10;
 
-		 //FONT_NAME   = new String("helvetica");
-
-        }
-     }
+    //FONT_NAME   = new String("helvetica");
+  }
+}
  
