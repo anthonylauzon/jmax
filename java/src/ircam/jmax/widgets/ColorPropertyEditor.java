d30f6871456d483fdefc37cb9a0d1c4f62f1201b
dechelle <dechelle>
1998-10-23 14:08:38 +0000

    Started the generic Bean editor. Work in progress...

---
+++ java/src/ircam/jmax/widgets/ColorPropertyEditor.java
@@ -0,0 +1,109 @@
+package ircam.jmax.widgets;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.*;
+
+import com.sun.java.swing.*;
+
+public class ColorPropertyEditor extends PropertyEditorSupport {
+
+  private JPanel panel;
+  private JPanel colorField;
+  private Color color;
+
+  static
+  {
+    String[] path = PropertyEditorManager.getEditorSearchPath();
+    String[] newPath = new String[ path.length + 1];
+
+    newPath[0] = "ircam.jmax.widgets";
+    System.arraycopy( path, 0, newPath, 1, path.length);
+
+    PropertyEditorManager.setEditorSearchPath( newPath);
+
+    for ( int i = 0; i < newPath.length; i++)
+      System.err.println( newPath[i]);
+
+    PropertyEditorManager.registerEditor( new java.awt.Color(10).getClass(),
+					  null);
+    PropertyEditorManager.registerEditor( new java.awt.Color(10).getClass(),
+					  new ircam.jmax.widgets.ColorPropertyEditor().getClass());
+  }
+
+  public ColorPropertyEditor()
+  {
+    color = Color.white;
+
+    panel = new JPanel();
+    panel.setLayout( new BoxLayout( panel, BoxLayout.X_AXIS));
+
+    colorField = new JPanel() {
+      public Dimension getPreferredSize()
+	{
+	  return getMinimumSize();
+	}
+      public Dimension getMinimumSize()
+	{
+	  return new Dimension( 15, 15);
+	}
+    };
+    colorField.setBackground( color);
+    panel.add( colorField);
+
+    JButton button = new JButton( "Choose");
+    button.addActionListener( new ActionListener() {
+      public void actionPerformed( ActionEvent e)
+	{
+	  color = JColorChooser.showDialog( panel, "Color Chooser", color);
+	}
+    });
+    panel.add( button);
+  }
+
+  public void setValue( Object value)
+  {
+    color = new Color( ((Color)value).getRGB());
+
+    colorField.setBackground( color);
+    colorField.repaint();
+  }
+
+  public Object getValue()
+  {
+    return color;
+  }
+
+  public boolean isPaintable()
+  {
+    return false;
+  }
+
+  public void paintValue( Graphics g, Rectangle r)
+  {
+  }
+
+  public String getAsText()
+  {
+    return "red";
+  }
+
+  public void setAsText(String text) throws IllegalArgumentException
+  {
+  }
+
+  public String[] getTags()
+  {
+    return null;
+  }
+
+  public Component getCustomEditor()
+  {
+    return panel;
+  }
+
+  public boolean supportsCustomEditor()
+  {
+    return true;
+  }
+}
