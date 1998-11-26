06051275b161767f18875c2cf8c557d3215846cb
schnell <schnell>
1998-11-26 09:58:51 +0000

    added new utilities

--- fts/src/lang/utils.h
+++ fts/src/lang/utils.h
@@ -5,5 +5,9 @@ extern fts_module_t fts_utils_module;
 
 #include "utils/hashtab.h"
 #include "utils/units.h"
+#include "utils/ramps.h"
+#include "utils/floatfuns.h"
+#include "utils/wto.h"
+#include "utils/ieeewrap.h"
 
 #endif
