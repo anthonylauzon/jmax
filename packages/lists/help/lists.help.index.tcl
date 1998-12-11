93563db3fc4dc73a9044e418f59d2bfecd882be5
schnell <schnell>
1998-12-11 09:38:22 +0000

    completed package

---
+++ packages/lists/help/lists.help.index.tcl
@@ -0,0 +1,12 @@
+helpPatch list $dir/list.help.jmax
+helpPatch listappend $dir/listappend.help.jmax
+helpPatch listcompose $dir/listcompose.help.jmax
+helpPatch listdecompose $dir/listdecompose.help.jmax
+helpPatch listelement $dir/listelement.help.jmax
+helpPatch listhead $dir/listhead.help.jmax
+helpPatch listlength $dir/listlength.help.jmax
+helpPatch listprepend $dir/listprepend.help.jmax
+helpPatch listreverse $dir/listreverse.help.jmax
+helpPatch lists $dir/lists.overview.jmax
+helpPatch listsublist $dir/listsublist.help.jmax
+helpPatch listtail $dir/listtail.help.jmax
