9af57fdcc68535d9438dd7cadc58ccd32a90d7c1
dechelle <dechelle>
1998-12-15 09:36:41 +0000

    *** empty log message ***

--- homepage/update.sh
+++ homepage/update.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 cp -f *.html /usr/local/www/ircam/equipes/temps-reel/jmax
 cp -rf images /usr/local/www/ircam/equipes/temps-reel/jmax
+cp -rf news /usr/local/www/ircam/equipes/temps-reel/jmax
