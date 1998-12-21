c8d23c7f66f1ead6cdbdf7612b88ef6500221c43
dechelle <dechelle>
1998-12-21 15:08:41 +0000

    Updated update.sh....

--- homepage/update.sh
+++ homepage/update.sh
@@ -1,4 +1,15 @@
 #! /bin/sh
-cp -f *.html /usr/local/www/ircam/equipes/temps-reel/jmax
-cp -rf images /usr/local/www/ircam/equipes/temps-reel/jmax
-cp -rf news /usr/local/www/ircam/equipes/temps-reel/jmax
+DSTDIR=/usr/local/www/ircam/equipes/temps-reel/jmax
+rm -rf ${DSTDIR}/*
+cat <<EOF > ${DSTDIR}/index.html
+<HTML>
+<BODY>
+<CENTER> <BLINK> <H1> Update in progress. Please wait ... </H1> </BLINK> </CENTER>
+</BODY>
+</HTML>
+EOF
+cd ${DSTDIR}
+cvs checkout max/homepage
+mv -f max/homepage/* .
+/bin/rm -rf CVS
+
