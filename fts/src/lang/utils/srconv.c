9ae15b9e9de310bf07d7c87fe5363a48d5c9c7ca
schnell <schnell>
1999-02-17 14:22:12 +0000

    added nice sample rate conversion routines

---
+++ fts/src/lang/utils/srconv.c
@@ -0,0 +1,170 @@
+#include "fts.h"
+
+#define FILTER_SIZE 24
+#define FILTER_OVER_BITS 11
+#define FILTER_OVER (1 << FILTER_OVER_BITS) /* 2048 */
+#define FTS_SRCONV_MAX_RATIO (double)10.0
+
+static double * restrict fts_srconv_filter = 0;
+
+/********************************************************
+ *
+ *  conversion filter
+ *
+ */
+
+static double *
+fts_srconv_make_filter(int size, int over)
+{
+  double *filter;
+  int i;
+ 
+  filter = (double *)fts_malloc(over * size * sizeof(double));
+
+  filter[0] = (double)1.0;
+
+  for(i=1; i<size*over; i++) 
+    {
+      double x = (double)i / (double)over;
+      double sinc = ((x == (double)0.0)? (double)1.0: sin(x * M_PI) / (x * M_PI));
+      double window = 0.54 + 0.46 * cos(M_PI * (double)i / (double)(size * over)); /* hamming */
+      
+      filter[i] = sinc * window;
+    }
+
+  return filter;
+}
+
+/********************************************************
+ *
+ *  conversion handle
+ *
+ */
+
+fts_srconv_t *
+fts_srconv_new(double ratio, int in_size, int channels)
+{
+  fts_srconv_t *srconv;
+  double inv_ratio = (double)1.0 / ratio;
+  int c;
+ 
+  if(ratio > FTS_SRCONV_MAX_RATIO || inv_ratio > FTS_SRCONV_MAX_RATIO)
+    {
+      fprintf(stderr, "srconv: can't change rate by more than a factor of %f\n", FTS_SRCONV_MAX_RATIO);
+      return 0;
+    }
+ 
+  if(channels > FTS_SRCONV_MAX_CHANNELS) 
+    {
+      fprintf(stderr, "srconv: can't change rate on more than %d channels\n", FTS_SRCONV_MAX_CHANNELS);
+      return 0;
+    }
+ 
+  srconv = (fts_srconv_t *)fts_malloc(sizeof(fts_srconv_t));
+
+  if(!fts_srconv_filter)
+    fts_srconv_filter = fts_srconv_make_filter(FILTER_SIZE, FILTER_OVER);
+
+  for(c=0; c<channels; c++)
+    {
+      int i;
+      
+      srconv->buffer[c] = (double *)fts_malloc((in_size + 2 * FILTER_SIZE) * sizeof(double));
+      if(!srconv->buffer[c]) 
+	{
+	  fprintf(stderr, "srconv: Allocation error!\n");
+	  return 0;
+	}
+      
+      for(i=0; i<2*FILTER_SIZE; i++)
+	srconv->buffer[c][i] = 0.0;
+    }
+  
+  srconv->in_size = in_size;
+  srconv->in_index = FILTER_SIZE;
+  srconv->channels = channels;
+  srconv->ratio = ratio;
+  srconv->filter_index = 0;
+
+  if(ratio >= (double)1.0)
+    {
+      /* up sampling */
+      srconv->incr = (int)((double)FILTER_OVER * inv_ratio + (double)0.5);
+      srconv->step = FILTER_OVER;
+      srconv->norm = 1.0;
+    }
+  else
+    {
+      /* down sampling */
+      srconv->incr = FILTER_OVER;
+      srconv->step = (int)((double)FILTER_OVER * ratio + (double)0.5);
+      srconv->norm = (double)srconv->step / (double)FILTER_OVER;
+    }
+    
+  return srconv;
+}
+
+void
+fts_srconv_delete(fts_srconv_t *srconv)
+{
+  int c;
+ 
+  for(c=0; c<srconv->channels; c++)
+    fts_free(srconv->buffer[c]);
+}
+
+int fts_srconv(fts_srconv_t * restrict srconv, float *in, float *out, int in_size, int out_size, int channels)
+{
+  int incr = srconv->incr;
+  int step = srconv->step;
+  double norm = srconv->norm;
+  int in_index;
+  int filter_index;
+  int out_index;
+  int c;
+ 
+  if(out_size <= 0)
+    return 0;
+
+  for(c=0; c<channels; c++)
+    {
+      double * restrict buffer = srconv->buffer[c];
+      int i;
+
+      in_index = srconv->in_index;
+      filter_index = srconv->filter_index;
+
+      /* copy input to convolution buffer (leaving the first 2*FILTER_SIZE points untouched) */
+      for (i=0; i<in_size; i++) 
+	buffer[2 * FILTER_SIZE + i] = in[c + i * channels];
+
+      /* convolution */
+      for(out_index=0; in_index<in_size+FILTER_SIZE && out_index<out_size; out_index++)
+	{
+	  double sum = buffer[in_index] * fts_srconv_filter[filter_index];
+
+	  for(i=1; i<FILTER_SIZE; i++) 
+	    {
+	      sum += 
+		buffer[in_index - i] * fts_srconv_filter[i * step + filter_index] +
+		buffer[in_index + i] * fts_srconv_filter[i * step - filter_index];
+	    }
+	  
+	  out[out_index * channels + c] = (float)(sum * norm);
+
+	  /* increment indices */ 
+	  filter_index += incr;
+	  in_index += filter_index / step;
+	  filter_index = filter_index % step; /* wrap filter index into first step */
+	}
+
+      /* copy convolution tail at beginning of buffer */
+      bcopy(buffer + in_size, buffer, 2 * FILTER_SIZE * sizeof(double));  
+    }
+  
+  srconv->in_index = in_index - in_size;
+  srconv->filter_index = filter_index;
+  
+  return out_index;
+}
+
---
+++ fts/src/lang/utils/srconv.h
@@ -0,0 +1,26 @@
+#ifndef _SRCONV_H_
+#define _SRCONV_H_
+
+#define FTS_SRCONV_MAX_CHANNELS 16
+
+typedef struct _fts_srconv
+{
+  double *buffer[FTS_SRCONV_MAX_CHANNELS];
+  int in_size;
+  int channels;
+  double ratio;
+  int in_index;
+  int filter_index;
+  int incr; /* increment for output sample rate (counted in filter resolution) */
+  int step; /* step of low-pass/interpolation filter coefficients (counted in filter resolution) */
+  double norm; /* normalization factor (for down sampling) */
+} fts_srconv_t;
+
+/* new/delete conversion buffer object */
+extern fts_srconv_t *fts_srconv_new(double ratio, int size, int n_channels);
+extern void fts_srconv_delete(fts_srconv_t *srconv);
+
+/* do conversion and return the number of output samples */
+extern int fts_srconv(fts_srconv_t *srconv, float *in, float *out, int size, int out_size, int n_channels);
+
+#endif
