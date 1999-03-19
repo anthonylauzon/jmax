1a3a96326a6448dfcd43346d20e13572e606b057
dechelle <dechelle>
1999-03-19 13:52:07 +0000

    Added MIDI in linux configuration

--- tcl/config/linuxpc.tcl
+++ tcl/config/linuxpc.tcl
@@ -12,12 +12,14 @@
 #  
 set jmaxArch linuxpc
 
-defineDevice analogIn  { oss_adc device /dev/audio } "OSS audio in"
+defineDevice analogIn  { oss_adc } "OSS audio in"
 defineDevice analogOut { oss_dac } "OSS audio out"
 
-defineDevice stereoIn  { oss_adc device /dev/audio } "OSS audio in"
-defineDevice stereoIn1 { oss_adc device /dev/audio } "OSS audio in"
+defineDevice stereoIn  { oss_adc } "OSS audio in"
+defineDevice stereoIn1 { oss_adc } "OSS audio in"
 
-defineDevice stereoOut  { oss_dac device /dev/audio } "OSS audio out"
-defineDevice stereoOut1 { oss_dac device /dev/audio } "OSS audio out"
+defineDevice stereoOut  { oss_dac } "OSS audio out"
+defineDevice stereoOut1 { oss_dac } "OSS audio out"
+
+defineDevice midi { oss_midi } "OSS MIDI"
 
