ecf1412af516c31f59f6e9eeb0f16bf85d9a277b
schnell <schnell>
1999-04-08 15:58:30 +0000

    updated distrib

--- fts/src/runtime/files/post.c
+++ fts/src/runtime/files/post.c
@@ -50,12 +50,9 @@ post_vector(int n, float *fp)
   post("\n");
 }
 
-#define MAX_POST_DATA_ATOMS 1024
-
 void
 post_atoms(int ac, const fts_atom_t *at)
 {
-  fts_atom_t a[MAX_POST_DATA_ATOMS];
   int i;
 
   for(i=0; i<ac; i++)
@@ -75,29 +72,10 @@ post_atoms(int ac, const fts_atom_t *at)
 	post("%f%s", fts_get_float(&at[i]), ps);
       else if (fts_is_ptr(&at[i]) )
 	post("%lx%s", (unsigned long) fts_get_ptr( &at[i]), ps);
-      else if (fts_is_data(&at[i]) )
-	{
-	  fts_data_t *data = fts_get_data(&at[i]);
-	  int size = fts_data_get_atoms(data, MAX_POST_DATA_ATOMS, a);
-
-	  if(fts_data_is_const(data))
-	    post("(const)");
-	  post("%s*{", fts_symbol_name(fts_data_get_class_name(data)));
-
-	  if(size <= MAX_POST_DATA_ATOMS)
-	    {
-	      post_atoms(size, a);
-	    }
-	  else
-	    {
-	      post_atoms(MAX_POST_DATA_ATOMS, a);
-	      post(" ... ");
-	    }
-
-	  post("}%s", ps);
-	}
       else if (fts_is_void(&at[i]))
 	post("(void)%s", ps);
+      else if (fts_is_data(&at[i]))
+	post("{<%s>}%s", fts_symbol_name(fts_data_get_class_name(fts_get_data(&at[i]))), ps);
       else
 	post("<%s>%s", fts_symbol_name(fts_get_type(&at[i])), ps);
     }
