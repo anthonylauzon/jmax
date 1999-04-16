3476c2236873fc65c6731fdcea0b4d9cb266addf
dechelle <dechelle>
1999-04-16 08:34:57 +0000

    Added thread module in runtime.

---
+++ fts/src/runtime.h
@@ -0,0 +1,27 @@
+/*
+ * jMax
+ * 
+ * Copyright (C) 1999 by IRCAM
+ * All rights reserved.
+ * 
+ * This program may be used and distributed under the terms of the 
+ * accompanying LICENSE.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY. See the LICENSE
+ * for DISCLAIMER OF WARRANTY.
+ * 
+ */
+#ifndef _RUNTIME_H_
+#define _RUNTIME_H_
+
+#include "runtime/STtools.h"
+#include "runtime/audio.h"
+#include "runtime/client.h"
+#include "runtime/devices.h"
+#include "runtime/files.h"
+#include "runtime/midi.h"
+#include "runtime/sched.h"
+#include "runtime/time.h"
+#include "runtime/thread.h"
+
+#endif
