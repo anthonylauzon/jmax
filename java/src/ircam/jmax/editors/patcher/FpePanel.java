c55308f8fb306b974a3e43658a99785105e19334
dececco <dececco>
1999-06-22 11:36:34 +0000

    Added copyrights and authors.

--- java/src/ircam/jmax/editors/patcher/FpePanel.java
+++ java/src/ircam/jmax/editors/patcher/FpePanel.java
@@ -3,7 +3,24 @@
 // 
 // Copyright (C) 1999 by IRCAM
 // All rights reserved.
+//
+// Authors: Maurizio De Cecco, Francois Dechelle, Enzo Maggi, Norbert Schnell
 // 
+//
+// jMax
+// 
+// Copyright (C) 1999 by IRCAM
+// All rights reserved.
+//
+// Authors: Maurizio De Cecco, Francois Dechelle, Enzo Maggi, Norbert Schnell
+// 
+// This program may be used and distributed under the terms of the 
+// accompanying LICENSE.
+//
+// This program is distributed WITHOUT ANY WARRANTY. See the LICENSE
+// for DISCLAIMER OF WARRANTY.
+// 
+//
 // This program may be used and distributed under the terms of the 
 // accompanying LICENSE.
 //
--- java/src/ircam/jmax/editors/patcher/ObjectAction.java
+++ java/src/ircam/jmax/editors/patcher/ObjectAction.java
@@ -1,3 +1,18 @@
+//
+// jMax
+// 
+// Copyright (C) 1999 by IRCAM
+// All rights reserved.
+//
+// Authors: Maurizio De Cecco, Francois Dechelle, Enzo Maggi, Norbert Schnell
+// 
+// This program may be used and distributed under the terms of the 
+// accompanying LICENSE.
+//
+// This program is distributed WITHOUT ANY WARRANTY. See the LICENSE
+// for DISCLAIMER OF WARRANTY.
+// 
+//
 package ircam.jmax.editors.patcher;
 
 import ircam.jmax.editors.patcher.objects.*;
