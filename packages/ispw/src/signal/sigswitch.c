9295b3cd259667fe587c2062388c6ecf589f8fc1
schnell <schnell>
1999-07-13 15:10:27 +0000

    added second inlet

--- packages/ispw/src/signal/sigswitch.c
+++ packages/ispw/src/signal/sigswitch.c
@@ -25,7 +25,6 @@
 #include "fts.h"
 
 extern ftl_program_t *dsp_chain_on;
-
 static fts_symbol_t switch_function = 0;
 
 static int switch_count = 1;
@@ -72,7 +71,7 @@ sigswitch_put(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_ato
     {
       fts_set_symbol(argv,   fts_dsp_get_input_name(dsp, 0));
       fts_set_symbol(argv+1, fts_dsp_get_output_name(dsp, 0));
-      fts_set_long  (argv+2, fts_dsp_get_input_size(dsp, 0));
+      fts_set_int(argv+2, fts_dsp_get_input_size(dsp, 0));
       dsp_add_funcall(ftl_sym.cpy.f, 3, argv);
     }
 }
@@ -87,10 +86,10 @@ sigswitch_put_after(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const f
 }
 
 static void
-sigswitch_int(fts_object_t *o, int winlet, fts_symbol_t is, int ac, const fts_atom_t *at)
+sigswitch_switch(fts_object_t *o, int winlet, fts_symbol_t is, int ac, const fts_atom_t *at)
 {
   sigswitch_t *this = (sigswitch_t *)o;
-  int state = fts_get_long(at);
+  int state = fts_get_int(at);
 
   ftl_data_copy( int, this->switch_ftl_data, &state);
 }
@@ -99,7 +98,7 @@ static void
 sigswitch_init(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
   sigswitch_t *this = (sigswitch_t *)o;
-  int state = 0;
+  int state = fts_get_int_arg(ac, at, 1, 0);
 
   this->switch_ftl_data = ftl_data_new( int);
   ftl_data_copy( int, this->switch_ftl_data, &state);
@@ -121,12 +120,11 @@ sigswitch_instantiate(fts_class_t *cl, int ac, const fts_atom_t *at)
 {
   fts_symbol_t a[1];
 
-  fts_class_init(cl, sizeof(sigswitch_t), 1, 1, 0);
+  fts_class_init(cl, sizeof(sigswitch_t), 2, 1, 0);
 
   a[0] = fts_s_symbol;
-  fts_method_define(cl, fts_SystemInlet, fts_s_init, sigswitch_init, 1, a);
-
-  fts_method_define(cl, fts_SystemInlet, fts_s_delete, sigswitch_delete, 0, 0);
+  fts_method_define_varargs(cl, fts_SystemInlet, fts_s_init, sigswitch_init);
+  fts_method_define_varargs(cl, fts_SystemInlet, fts_s_delete, sigswitch_delete);
 
   a[0] = fts_s_ptr;  
   fts_method_define(cl, fts_SystemInlet, fts_s_put, sigswitch_put, 1, a);
@@ -134,7 +132,8 @@ sigswitch_instantiate(fts_class_t *cl, int ac, const fts_atom_t *at)
   fts_method_define(cl, fts_SystemInlet, fts_new_symbol("put_after_successors"), sigswitch_put_after, 0, 0);
   
   a[0] = fts_s_int;
-  fts_method_define(cl, 0, fts_s_int, sigswitch_int, 1, a);
+  fts_method_define(cl, 0, fts_s_int, sigswitch_switch, 1, a);
+  fts_method_define(cl, 1, fts_s_int, sigswitch_switch, 1, a);
   
   dsp_sig_inlet(cl, 0);
   dsp_sig_outlet(cl, 0);
