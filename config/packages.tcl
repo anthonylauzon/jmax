0e6dcebbeeaef2995335b37967a4e2dfa8ab1413
dechelle <dechelle>
1999-07-19 11:07:03 +0000

    Fixed remaining headers.

--- config/packages.tcl
+++ config/packages.tcl
@@ -1,49 +1,57 @@
-#
-#  jMax
-#  
-#  Copyright (C) 1999 by IRCAM
-#  All rights reserved.
-#  
-#  Authors: Maurizio De Cecco, Francois Dechelle, Enzo Maggi, Norbert Schnell.
-#
-#  This program may be used and distributed under the terms of the 
-#  accompanying LICENSE.
 # 
-#  This program is distributed WITHOUT ANY WARRANTY. See the LICENSE
-#  for DISCLAIMER OF WARRANTY.
-#  
+# jMax
+# Copyright (C) 1999 by IRCAM
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# See file LICENSE for further informations on licensing terms.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# 
+# Authors: Maurizio De Cecco, Francois Dechelle, Enzo Maggi, Norbert Schnell.
+#
+
 
 ##
 ## Packages installed by default in this site
 ##
 
 # Max 0.26 compatibilty package
+
 package require ispw
 
+
 # Table object and editor
+
 package require table
 
+
 # Qlist object and editor
+
 package require qlist
 
+
 # Explode object and editor
+
 package require explode
 
-# new control flow classes
-package require control
 
-# new message classes
-package require mess
+# List handling package
 
-# new atom classes
-package require generic
-
-# new numeric classes
-package require numeric
-
-# List handling classes
 package require lists
 
+
 # I/O package
+
 package require io
 
