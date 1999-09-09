f9012f23e8765a8d29f21864e2714df68055f605
schnell <schnell>
1999-09-09 11:15:16 +0000

    fixed atom_array

--- fts/src/lang/mess/atoms.h
+++ fts/src/lang/mess/atoms.h
@@ -60,116 +60,97 @@
  */
 
 /* An initializer for empty atoms */
-
 #define FTS_NULL {(fts_symbol_t)0, {0}}
 
 /* A variable initialized to an empty atom */
-
 extern const fts_atom_t fts_null;
 
 /* Macros to deal with any type */
+#define fts_set_type(ap, ttype) (((ap)->type) = (ttype))
+#define fts_get_type(ap) ((ap)->type)
+#define fts_is_a(ap, ttype) (((ap)->type) == (ttype))
 
-#define fts_set_type(ap, ttype)    (((ap)->type) = (ttype))
-#define fts_get_type(ap)           ((ap)->type)
-#define fts_is_a(ap, ttype)        (((ap)->type) == (ttype))
+#define fts_get_selector(ap) (fts_type_get_selector((ap)->type))
 
 /* Return a pointer; i.e. you can write "*(fts_atom_value(a)).fts_symbol = fts_s_int;" for example */
-
 #define fts_atom_value(ap)          (&((ap)->value))
 
-
-/* specialized macros for predefined types */
-
-#define fts_set_symbol(ap, x)       \
+/* set atoms of predefined types */
+#define fts_set_symbol(ap, x) \
         do {fts_set_type(ap, fts_s_symbol); fts_word_set_symbol(fts_atom_value(ap), (x));} while (0)
-
-#define fts_set_string(ap, x)       \
+#define fts_set_string(ap, x) \
         do {fts_set_type(ap, fts_s_string); fts_word_set_string(fts_atom_value(ap), (x));} while (0)
-
-#define fts_set_ptr(ap, x)         \
+#define fts_set_ptr(ap, x) \
         do {fts_set_type(ap, fts_s_ptr); fts_word_set_ptr(fts_atom_value(ap), (x));} while (0)
-
-#define fts_set_fun(ap, x)         \
+#define fts_set_fun(ap, x) \
         do {fts_set_type(ap, fts_s_fun); fts_word_set_fun(fts_atom_value(ap), (x));} while (0)
-
-#define fts_set_int(ap, x)         \
+#define fts_set_int(ap, x) \
      do {fts_set_type(ap, fts_s_int); fts_word_set_int(fts_atom_value(ap), (x));} while (0)
-
-#define fts_set_long(ap, x)        fts_set_int(ap, x)
-
-#define fts_set_float(ap, x)       \
+#define fts_set_long(ap, x) fts_set_int(ap, x)
+#define fts_set_float(ap, x) \
      do {fts_set_type(ap, fts_s_float); fts_word_set_float(fts_atom_value(ap), (x));} while (0)
-
-#define fts_set_object(ap, x)       \
+#define fts_set_object(ap, x) \
      do {fts_set_type(ap, fts_s_object); fts_word_set_object(fts_atom_value(ap), (x));} while (0)
-
 #define fts_set_connection(ap, x)       \
      do {fts_set_type(ap, fts_s_connection); fts_word_set_connection(fts_atom_value(ap), (x));} while (0)
-
-#define fts_set_data(ap, x)       \
+#define fts_set_data(ap, x) \
      do {fts_set_type(ap, fts_s_data); fts_word_set_data( fts_atom_value(ap), (x));} while (0)
+/* atom_array atoms */
+#define fts_set_atom_array(ap, x) \
+     do {fts_set_type(ap, fts_s_atom_array); fts_word_set_data(fts_atom_value(ap), ((fts_data_t *)x));} while (0)
 
-#define fts_set_atom_array(ap, x)       \
-     do {fts_set_type(ap, fts_s_atom_array); fts_word_set_atom_array( fts_atom_value(ap), (x));} while (0)
+#define fts_set_void(ap) (fts_set_type(ap, fts_s_void))
+#define fts_set_error(ap) (fts_set_type(ap, fts_s_error))
+#define fts_set_true(ap) (fts_set_type(ap, fts_s_true))
+#define fts_set_false(ap) (fts_set_type(ap, fts_s_false))
 
-#define fts_set_void(ap)           (fts_set_type(ap, fts_s_void))
-#define fts_set_error(ap)           (fts_set_type(ap, fts_s_error))
+/* get values of atoms of predefined types */
+#define fts_get_symbol(ap) (fts_word_get_symbol(fts_atom_value(ap)))
+#define fts_get_string(ap) (fts_word_get_string(fts_atom_value(ap)))
+#define fts_get_ptr(ap) (fts_word_get_ptr(fts_atom_value(ap)))
+#define fts_get_fun(ap) (fts_word_get_fun(fts_atom_value(ap)))
+#define fts_get_int(ap) (fts_word_get_int(fts_atom_value(ap)))
+#define fts_get_long(ap) fts_get_int(ap)
+#define fts_get_float(ap) (fts_word_get_float(fts_atom_value(ap)))
+#define fts_get_number_int(ap) (fts_is_a(ap, fts_s_float) ? (int)fts_get_float(ap) : fts_get_int(ap))
+#define fts_get_number_float(ap) (fts_is_a(ap, fts_s_float) ? fts_get_float(ap) : (float)fts_get_int(ap))
+#define fts_get_double(ap) (fts_is_a(ap, fts_s_float) ? (double) fts_get_float(ap) : (double)fts_get_int(ap))
+#define fts_get_object(ap) (fts_word_get_object(fts_atom_value(ap)))
+#define fts_get_connection(ap) (fts_word_get_connection(fts_atom_value(ap)))
+#define fts_get_data(ap) (fts_word_get_data(fts_atom_value(ap)))
+#define fts_get_atom_array(ap) ((fts_atom_array_t *)fts_word_get_data(fts_atom_value(ap)))
 
-#define fts_set_true(ap)           (fts_set_type(ap, fts_s_true))
-#define fts_set_false(ap)          (fts_set_type(ap, fts_s_false))
-
-
-#define fts_get_symbol(ap)         (fts_word_get_symbol(fts_atom_value(ap)))
-#define fts_get_string(ap)         (fts_word_get_string(fts_atom_value(ap)))
-#define fts_get_ptr(ap)            (fts_word_get_ptr(fts_atom_value(ap)))
-#define fts_get_fun(ap)            (fts_word_get_fun(fts_atom_value(ap)))
-#define fts_get_int(ap)            (fts_word_get_int(fts_atom_value(ap)))
-#define fts_get_long(ap)           fts_get_int(ap)
-#define fts_get_float(ap)          (fts_word_get_float(fts_atom_value(ap)))
-#define fts_get_number_int(ap)     (fts_is_a(ap, fts_s_float) ? (int)fts_get_float(ap) : fts_get_int(ap))
-#define fts_get_number_float(ap)   (fts_is_a(ap, fts_s_float) ? fts_get_float(ap) : (float)fts_get_int(ap))
-
-#define fts_get_double(ap)         (fts_is_a(ap, fts_s_float) ? (double) fts_get_float(ap) : (double)fts_get_int(ap))
-
-#define fts_get_object(ap)         (fts_word_get_object(fts_atom_value(ap)))
-#define fts_get_connection(ap)     (fts_word_get_connection(fts_atom_value(ap)))
-#define fts_get_data(ap)           (fts_word_get_data(fts_atom_value(ap)))
-#define fts_get_atom_array(ap)     (fts_word_get_atom_array(fts_atom_value(ap)))
-
-#define fts_is_void(ap)            fts_is_a(ap, fts_s_void)
-#define fts_is_error(ap)           fts_is_a(ap, fts_s_error)
-#define fts_is_symbol(ap)          fts_is_a(ap, fts_s_symbol)
-#define fts_is_string(ap)          fts_is_a(ap, fts_s_string)
-#define fts_is_ptr(ap)             fts_is_a(ap, fts_s_ptr)
-#define fts_is_fun(ap)             fts_is_a(ap, fts_s_fun)
-#define fts_is_int(ap)             fts_is_a(ap, fts_s_int)
-#define fts_is_long(ap)            fts_is_int(ap)
-#define fts_is_float(ap)           fts_is_a(ap, fts_s_float)
-#define fts_is_number(ap)          (fts_is_a(ap, fts_s_int) || fts_is_a(ap, fts_s_float))
-#define fts_is_object(ap)          fts_is_a(ap, fts_s_object)
-#define fts_is_connection(ap)      fts_is_a(ap, fts_s_connection)
-#define fts_is_true(ap)            fts_is_a(ap, fts_s_true)
-#define fts_is_false(ap)           fts_is_a(ap, fts_s_false)
+/* check atoms for predefined types */
+#define fts_is_void(ap) fts_is_a(ap, fts_s_void)
+#define fts_is_error(ap) fts_is_a(ap, fts_s_error)
+#define fts_is_symbol(ap) fts_is_a(ap, fts_s_symbol)
+#define fts_is_string(ap) fts_is_a(ap, fts_s_string)
+#define fts_is_ptr(ap) fts_is_a(ap, fts_s_ptr)
+#define fts_is_fun(ap) fts_is_a(ap, fts_s_fun)
+#define fts_is_int(ap) fts_is_a(ap, fts_s_int)
+#define fts_is_long(ap) fts_is_int(ap)
+#define fts_is_float(ap) fts_is_a(ap, fts_s_float)
+#define fts_is_number(ap) (fts_is_a(ap, fts_s_int) || fts_is_a(ap, fts_s_float))
+#define fts_is_object(ap) fts_is_a(ap, fts_s_object)
+#define fts_is_connection(ap) fts_is_a(ap, fts_s_connection)
+#define fts_is_true(ap) fts_is_a(ap, fts_s_true)
+#define fts_is_false(ap) fts_is_a(ap, fts_s_false)
 #define fts_is_data(ap) (fts_is_a(ap, fts_s_data))
+#define fts_is_atom_array(ap) (fts_is_a(ap, fts_s_atom_array))
 
 /* Convenience macros to deal with type and atom algebra */
-
-#define fts_same_types(ap1, ap2)    (((ap1)->type) == (ap2)->type)
+#define fts_same_types(ap1, ap2) (((ap1)->type) == (ap2)->type)
 
 /* equality test between two atoms */
-
 extern int fts_atom_are_equals(const fts_atom_t *a1, const fts_atom_t *a2);
 
 /* null test: a null content can be a null pointer or a zero value */
-
 extern int fts_atom_is_null(const fts_atom_t *a);
 
 /* Subsequence  testing */
-
 extern int fts_atom_is_subsequence(int sac, const fts_atom_t *sav, int ac, const fts_atom_t *av);
 
 /* Atom printing function, usually for debug */
-
 void fprintf_atoms(FILE *f, int ac, const fts_atom_t *at);
 void sprintf_atoms(char *s, int ac, const fts_atom_t *at);
 
