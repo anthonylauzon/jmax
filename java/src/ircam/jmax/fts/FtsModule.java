9672a9451d9c667e5b11962f962d3e25a60ecde1
schnell <schnell>
1999-09-20 15:21:42 +0000

    dirty hack: declared float vector under "ivec"

--- java/src/ircam/jmax/fts/FtsModule.java
+++ java/src/ircam/jmax/fts/FtsModule.java
@@ -37,34 +37,28 @@ public class FtsModule
   static public void initModule()
   {
     // Install the MDA entities
-
     Mda.installDocumentHandler( new FtsDotPatRemoteDocumentHandler());
     Mda.installDocumentHandler( new FtsBmaxRemoteDocumentHandler());
 
     // Not yet
     // Mda.installDocumentHandler( new FtsBmaxRemoteDocumentHandler());
-
     Fts.setPatcherDocumentType(new FtsPatcherDocumentType());
     Mda.installDocumentType(Fts.getPatcherDocumentType());
 
     // Install the FTS tcl commands
-
     ircam.jmax.fts.tcl.TclFtsPackage.installPackage();
 
     // Install the Integer vector class
-
-    Fts.registerRemoteDataClass("integer_vector", ircam.jmax.fts.FtsIntegerVector.class);
+    Fts.registerRemoteDataClass("ivec", ircam.jmax.fts.FtsIntegerVector.class);
+    Fts.registerRemoteDataClass("fvec", ircam.jmax.fts.FtsIntegerVector.class);
 
     // Install the Object set class
-
     Fts.registerRemoteDataClass("object_set_data", ircam.jmax.fts.FtsObjectSet.class);
 
     // Install the atom list class
-
     Fts.registerRemoteDataClass("atom_list_data", ircam.jmax.fts.FtsAtomList.class);
 
     // Install the patcher data class
-
     Fts.registerRemoteDataClass("patcher_data", ircam.jmax.fts.FtsPatcherData.class);
   }
 }
