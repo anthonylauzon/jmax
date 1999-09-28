fea0ae9577f7d2f9621ddb224634a4d9f48827ff
hanappe <hanappe>
1999-09-28 15:13:23 +0000

    Adding package Scheme init and help scripts (control+help, explode+help, generic+help, guiobj+help)

---
+++ packages/guiobj/help/guiobj.help.index.scm
@@ -0,0 +1,9 @@
+;; gui
+
+(help-patch "button" (file-cat dir "help" "button.help.jmax"))
+(help-patch "messbox" (file-cat dir "help" "messbox.help.jmax"))
+(help-patch "intbox" (file-cat dir "help" "intbox.help.jmax"))
+(help-patch "floatbox" (file-cat dir "help" "floatbox.help.jmax"))
+(help-patch "slider" (file-cat dir "help" "slider.help.jmax"))
+(help-patch "toggle" (file-cat dir "help" "toggle.help.jmax"))
+
