a19d597c9a2e15e97357d7a3cb7cc857a78f63f0
hanappe <hanappe>
1999-09-28 15:15:09 +0000

    Adding package Scheme init and help scripts (control+help, explode+help, generic+help, guiobj+help(2x), io, ispw+help, lists)

---
+++ packages/lists/help/lists.help.index.scm
@@ -0,0 +1,38 @@
+; 
+; jMax
+; Copyright (C) 1994, 1995, 1998, 1999 by IRCAM-Centre Georges Pompidou, Paris, France.
+; 
+; This program is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License
+; as published by the Free Software Foundation; either version 2
+; of the License, or (at your option) any later version.
+; 
+; See file LICENSE for further informations on licensing terms.
+; 
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+; 
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+; 
+; Based on Max/ISPW by Miller Puckette.
+;
+; Authors: Maurizio De Cecco, Francois Dechelle, Enzo Maggi, Norbert Schnell.
+;
+
+(help-patch "list" (file-cat dir "help" "list.help.jmax"))
+(help-patch "listappend" (file-cat dir "help" "listappend.help.jmax"))
+(help-patch "listcompose" (file-cat dir "help" "listcompose.help.jmax"))
+(help-patch "listdecompose" (file-cat dir "help" "listdecompose.help.jmax"))
+(help-patch "listelement" (file-cat dir "help" "listelement.help.jmax"))
+(help-patch "listhead" (file-cat dir "help" "listhead.help.jmax"))
+(help-patch "listlength" (file-cat dir "help" "listlength.help.jmax"))
+(help-patch "listprepend" (file-cat dir "help" "listprepend.help.jmax"))
+(help-patch "listreverse" (file-cat dir "help" "listreverse.help.jmax"))
+(help-patch "lists" (file-cat dir "help" "lists.overview.jmax"))
+(help-patch "listsublist" (file-cat dir "help" "listsublist.help.jmax"))
+(help-patch "listtail" (file-cat dir "help" "listtail.help.jmax"))
+(help-summary "Lists" (file-cat dir "help" "lists.summary.jmax"))
