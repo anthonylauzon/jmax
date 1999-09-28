94cc533186a7f7f082a8f31ba621e3a2ea5a94db
hanappe <hanappe>
1999-09-28 15:15:59 +0000

    Adding package Scheme init and help scripts (control+help, explode+help, generic+help, guiobj+help, io, ispw+help, lists+help, mess+help, numeric+help)

---
+++ packages/numeric/help/numeric.help.index.scm
@@ -0,0 +1,6 @@
+(help-patch "itab" (file-cat dir "help" "ivec.help.jmax"))
+(help-patch "ftab" (file-cat dir "help" "fvec.help.jmax"))
+(help-patch "wrap" (file-cat dir "help" "wrap.help.jmax"))
+(help-patch "rewrap" (file-cat dir "help" "rewrap.help.jmax"))
+(help-patch "nsplit" (file-cat dir "help" "nsplit.help.jmax"))
+
