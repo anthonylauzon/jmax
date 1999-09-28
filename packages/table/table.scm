ec032b1befce4e5ddcc9615b03e6ff1eb41d75ac
hanappe <hanappe>
1999-09-28 15:16:45 +0000

    Adding package Scheme init and help scripts (control+help, explode+help, generic+help, guiobj+help, io, ispw+help, lists+help, mess+help, numeric+help, qlist, reftype, system, table)

---
+++ packages/table/table.scm
@@ -0,0 +1,36 @@
+; 
+; jMax
+; Copyright (C) 1999 by IRCAM
+; 
+; This program is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License
+; as published by the Free Software Foundation; either version 2
+; of the License, or (at your option) any later version.
+; 
+; See file LICENSE for further informations on licensing terms.
+; 
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+; 
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+; 
+; Authors: Maurizio De Cecco, Francois Dechelle, Enzo Maggi, Norbert Schnell.
+;
+
+;; package declaration
+(provide-package "table" "0.0.0")
+
+;; Load the table class
+;; The table extension will install
+;; the data type and handler
+;;
+(append-local-path this-package (file-cat "java" "classes"))
+(load-class this-package "ircam.jmax.editors.table.TableExtension")
+(println "package: table (integer table editor)")
+
+
+
