926ff60650376776deab187030a4760d8ed3448f
hanappe <hanappe>
1999-09-28 15:12:10 +0000

    Adding package Scheme init and help scripts

---
+++ packages/control/help/control.help.index.scm
@@ -0,0 +1,3 @@
+(help-patch "switch" (file-cat dir "help" "switch.help.jmax"))
+(help-patch "demux" (file-cat dir "help" "demux.help.jmax"))
+
---
+++ packages/generic/help/generic.help.index.scm
@@ -0,0 +1,2 @@
+(help-patch "mat" (file-cat dir "help" "mat.help.jmax"))
+(help-patch "getlist" (file-cat dir "help" "getlist.help.jmax"))
