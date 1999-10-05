2c9983bc7bc9586ac8a7c7f6e09391ceac4a98fb
schnell <schnell>
1999-10-05 08:32:39 +0000

    new package ispwmath

---
+++ packages/ispwmath/help/ispwmath.help.index.tcl
@@ -0,0 +1,27 @@
+## arithmetics (6)
+helpPatch + $dir/arith.help.jmax
+helpPatch - $dir/arith.help.jmax
+helpPatch * $dir/arith.help.jmax
+helpPatch / $dir/arith.help.jmax
+helpPatch inv* $dir/arith.help.jmax
+helpPatch inv+ $dir/arith.help.jmax
+helpPatch % $dir/arith.help.jmax
+
+## comparison operators (6)
+helpPatch <= $dir/compare.help.jmax
+helpPatch == $dir/compare.help.jmax
+helpPatch > $dir/compare.help.jmax
+helpPatch >= $dir/compare.help.jmax
+helpPatch != $dir/compare.help.jmax
+helpPatch < $dir/compare.help.jmax
+
+## logic operators (4)
+helpPatch | $dir/logic.help.jmax
+helpPatch || $dir/logic.help.jmax
+helpPatch && $dir/logic.help.jmax
+helpPatch & $dir/logic.help.jmax
+
+##shifting operators (2)
+helpPatch << $dir/shift.help.jmax
+helpPatch >> $dir/shift.help.jmax
+
