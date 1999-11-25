bf8e6dc0a79d44ffdfd2b8fee327998a1689f67a
dechelle <dechelle>
1999-11-25 16:43:56 +0000

    Removed sysinit.h

--- fts/src/sys.h
+++ fts/src/sys.h
@@ -37,14 +37,12 @@
   Generic include file for the sys layer.
   */
 
-#include "sys/sysinit.h"
 #include "sys/hw.h"
 #include "sys/status.h"
 #include "sys/platform.h"
 #include "sys/mem.h"
 #include "sys/modules.h"
 #include "sys/version.h"
-#include "sys/welcome.h"
 
 #endif
 
