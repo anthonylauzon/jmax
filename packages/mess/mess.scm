e70a46ef92cdad0de31ec52f5be1ee43fb83dbbd
hanappe <hanappe>
1999-12-07 17:32:43 +0000

    Moving to new package settings files

---
+++ packages/mess/mess.scm
@@ -0,0 +1,17 @@
+;;
+;;  jMax
+;;  
+;;  Copyright (C) 1999 by IRCAM
+;;  All rights reserved.
+;;  
+;;  This program may be used and distributed under the terms of the 
+;;  accompanying LICENSE.
+;; 
+;;  This program is distributed WITHOUT ANY WARRANTY. See the LICENSE
+;;  for DISCLAIMER OF WARRANTY.
+;;  
+
+(load-silently (file-cat dir "help" "mess.help.index.scm"))
+(help-summary "Mess" (file-cat dir "help" "mess.summary.jmax"))
+
+
