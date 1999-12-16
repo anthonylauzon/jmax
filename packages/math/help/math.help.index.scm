fe6e6b3b5780a5a44fda5e1735e8f63087bc7a9f
dechelle <dechelle>
1999-12-16 11:17:13 +0000

    Added scheme file

---
+++ packages/math/help/math.help.index.scm
@@ -0,0 +1,23 @@
+(help-patch "+" (file-cat dir "help" "arithmetic.help.jmax"))
+(help-patch "-" (file-cat dir "help" "arithmetic.help.jmax"))
+(help-patch "*" (file-cat dir "help" "arithmetic.help.jmax"))
+(help-patch "/" (file-cat dir "help" "arithmetic.help.jmax"))
+(help-patch "-+" (file-cat dir "help" "arithmetic.help.jmax"))
+(help-patch "/*" (file-cat dir "help" "arithmetic.help.jmax"))
+
+(help-patch "<=" (file-cat dir "help" "comparison.help.jmax"))
+(help-patch "==" (file-cat dir "help" "comparison.help.jmax"))
+(help-patch ">" (file-cat dir "help" "comparison.help.jmax"))
+(help-patch ">=" (file-cat dir "help" "comparison.help.jmax"))
+(help-patch "!=" (file-cat dir "help" "comparison.help.jmax"))
+(help-patch "<" (file-cat dir "help" "comparison.help.jmax"))
+
+;;(help-patch "||" (file-cat dir "help" "boolean.help.jmax"))
+;;(help-patch "&&" (file-cat dir "help" "boolean.help.jmax"))
+
+;;(help-patch "|" (file-cat dir "help" "logic.help.jmax"))
+;;(help-patch "&" (file-cat dir "help" "logic.help.jmax"))
+
+;;(help-patch "<<" (file-cat dir "help" "shift.help.jmax"))
+;;(help-patch ">>" (file-cat dir "help" "shift.help.jmax"))
+
