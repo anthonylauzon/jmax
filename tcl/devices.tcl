850b91d2700c041f61701b7510978b87ff228b5c
borghesi <borghesi>
2000-02-08 14:05:45 +0000

    *** empty log message ***

--- tcl/devices.tcl
+++ tcl/devices.tcl
@@ -32,7 +32,7 @@
 
 proc defineDevice {name ucscode doc } {
     global jmaxDeviceTable
-    global jmaxDeviceDoc
+    global jmaxDeviceDoc   
 
     set jmaxDeviceTable($name) $ucscode
     set jmaxDeviceDoc($name)   $doc
@@ -41,29 +41,29 @@ proc defineDevice {name ucscode doc } {
 proc resetAudioIn {} {
     ucs reset device in~
 }
-proc openAudioIn { name device} {
+proc openAudioIn { name device {args ""}} {
     global jmaxDeviceTable
     global jmaxDeviceDoc
 
     puts "Opening audio in $name: $jmaxDeviceDoc($device)"
-    eval [concat " ucs open device in~ $name as " $jmaxDeviceTable($device) ]
+    eval [concat " ucs open device in~ $name as " $jmaxDeviceTable($device) $args]
 }
 
-proc openDefaultAudioIn { device } {
+proc openDefaultAudioIn { device {args ""}} {
     global jmaxDeviceTable
     global jmaxDeviceDoc
 
     puts "Opening default audio in: $jmaxDeviceDoc($device)"
-    eval [concat " ucs open device in~ __defaultAudioIn as " $jmaxDeviceTable($device) ]
+    eval [concat " ucs open device in~ __defaultAudioIn as " $jmaxDeviceTable($device) $args]
     ucs default in~ __defaultAudioIn
 }
 
-proc openNamedDefaultAudioIn { name device } {
+proc openNamedDefaultAudioIn { name device {args ""} } {
     global jmaxDeviceTable
     global jmaxDeviceDoc
 
     puts "Opening audio in $name (also default): $jmaxDeviceDoc($device)"
-    eval [concat " ucs open device in~ $name as " $jmaxDeviceTable($device) ]
+    eval [concat " ucs open device in~ $name as " $jmaxDeviceTable($device) $args]
     ucs default in~ $name
 }
 
@@ -71,30 +71,30 @@ proc resetAudioOut {} {
     ucs reset device out~
 }
 
-proc openAudioOut { name device } {
+proc openAudioOut { name device {args ""} } {
     global jmaxDeviceTable
     global jmaxDeviceDoc
 
 
     puts "Opening audio out $name: $jmaxDeviceDoc($device)"
-    eval [concat " ucs open device out~ $name as " $jmaxDeviceTable($device) ]
+    eval [concat " ucs open device out~ $name as " $jmaxDeviceTable($device) $args]
 }
 
-proc openDefaultAudioOut { device } {
+proc openDefaultAudioOut { device {args ""} } {
     global jmaxDeviceTable
     global jmaxDeviceDoc
 
     puts "Opening default audio out: $jmaxDeviceDoc($device)"
-    eval [concat " ucs open device out~ __defaultAudioOut as " $jmaxDeviceTable($device)]
+    eval [concat " ucs open device out~ __defaultAudioOut as " $jmaxDeviceTable($device) $args]
     ucs default out~ __defaultAudioOut
 }
 
-proc openNamedDefaultAudioOut { name device  } {
+proc openNamedDefaultAudioOut { name device {args ""} } {
     global jmaxDeviceTable
     global jmaxDeviceDoc
 
     puts "Opening audio out $name (also default): $jmaxDeviceDoc($device)"
-    eval [concat " ucs open device out~ $name as " $jmaxDeviceTable($device) ]
+    eval [concat " ucs open device out~ $name as " $jmaxDeviceTable($device) $args]
     ucs default out~ $name
 }
 
@@ -102,24 +102,32 @@ proc resetMidi {} {
     ucs reset device midi
 }
 
-proc openMidi  { num device } {
+proc openMidi  { num device {args ""} } {
     global jmaxDeviceTable
     global jmaxDeviceDoc
 
     puts "Opening midi port $num: $jmaxDeviceDoc($device)"
-    eval [concat " ucs open device midi $num as " $jmaxDeviceTable($device) ]
+    eval [concat " ucs open device midi $num as " $jmaxDeviceTable($device) $args]
 }
 
-proc openDefaultMidi  { device } {
-    global jmaxDeviceTable
-    global jmaxDeviceDoc
+proc openDefaultMidi  { device {args ""} } {
+   global jmaxDeviceTable
+   global jmaxDeviceDoc
 
-    puts "Opening default midi port: $jmaxDeviceDoc($device)"
-    eval [concat " ucs open device midi 0 as " $jmaxDeviceTable($device) ]
+   puts "Opening default midi port: $jmaxDeviceDoc($device)"
+   eval [concat " ucs open device midi 0 as " $jmaxDeviceTable($device) $args]
 }
+
 
 ##
 ## Builtin  devices
 ## 
 
 defineDevice profile {prof_dac channels 2} "Profile Dac"
+
+
+
+
+
+
+
