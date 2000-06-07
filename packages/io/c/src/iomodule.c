9a44726f964434b9c957cb105a475e93f5b4e44b
dechelle <dechelle>
2000-06-07 19:32:40 +0000

    *** empty log message ***

--- packages/io/c/src/iomodule.c
+++ packages/io/c/src/iomodule.c
@@ -28,12 +28,14 @@
 
 extern void udp_config(void);
 extern void udpmessage_config(void);
+extern void serial_config( void);
 
 static void
 fts_io_init(void)
 {
   udp_config();
   udpmessage_config();
+  serial_config();
 }
 
 fts_module_t io_module = {"io", "I/O objects", fts_io_init, 0, 0};
