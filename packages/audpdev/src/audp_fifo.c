6a14fa0493522f5983aa43d9153ad216e30c6ba1
dechelle <dechelle>
2000-12-05 15:41:26 +0000

    Added audpdev

---
+++ packages/audpdev/src/audp_fifo.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include "fts.h"
+#include "audp_fifo.h"
+
+
+int audp_fifo_init(audp_fifo_t *fifo, unsigned long size, unsigned long low, unsigned long high) {
+  fifo->size = size;
+  if ((fifo->buf = fts_malloc(size)) == NULL)
+    return -1;
+
+  fifo->begin = fifo->end = 0;
+  fifo->low = low;
+  fifo->high = high;
+
+  return 0;
+}
+
+
+int audp_fifo_write(audp_fifo_t *fifo, unsigned long size, void *buf) {
+  unsigned long fifo_fill = fifo->end - fifo->begin;
+
+  if (fifo_fill + size > fifo->size) {
+    /* not enough space */
+    return -1;
+  }
+  
+  if (fifo->end + size > fifo->size) {
+    /* we have to relocate fifo data at begining */
+    unsigned long i,j;
+
+    for (i=fifo->begin,j=0; j<fifo_fill; i++, j++)
+      fifo->buf[j] = fifo->buf[i];
+    
+    fifo->begin = 0;
+    fifo->end = fifo_fill;
+  }
+
+  memcpy(fifo->buf + fifo->end, buf, size);
+  fifo->end += size;
+
+  return size;
+}
+
+int audp_fifo_read(audp_fifo_t *fifo, unsigned long size, void *buf) {
+  if (fifo->end - fifo->begin < size) 
+    return -1;
+  
+  memcpy(buf, fifo->buf + fifo->begin, size);
+  fifo->begin += size;
+  return size;
+}
+  
+
+
+    
---
+++ packages/audpdev/src/udp.h
@@ -0,0 +1,7 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define MAX_UDP_SIZE 20000
