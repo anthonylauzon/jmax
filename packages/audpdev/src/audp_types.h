5e006b53fa72c0e17ae38c44ff5d092d3f7537ac
dechelle <dechelle>
2000-12-21 15:34:03 +0000

    Added missing file

---
+++ packages/audpdev/src/audp_types.h
@@ -0,0 +1,17 @@
+/*---------------audp_packet.h--------------
+
+Description:
+-----------
+   Type definition for the basic audp elements
+
+--------------------------------------------*/
+
+typedef unsigned char	audp_uchar_t;   /* audp 8bits unsigned char type */
+typedef short		audp_int16_t;   /* audp 16bits signed integer type */ 
+typedef unsigned short	audp_uint16_t;  /* audp 16bits unsigned integer type */
+typedef int		audp_int32_t;   /* audp 32bits signed integer type */
+typedef unsigned int	audp_uint32_t;  /* audp 32bits unsigned integer type */
+typedef float		audp_float32_t; /* audp 32bits float type */
+
+
+
