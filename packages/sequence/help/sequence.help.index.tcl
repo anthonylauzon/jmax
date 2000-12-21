82c3ffd7555b4e522338a270c8f4831c182d5ca2
dechelle <dechelle>
2000-12-21 18:28:56 +0000

    Re-incorporating the changes for 2.4.17

---
+++ packages/sequence/help/sequence.help.index.tcl
@@ -0,0 +1,5 @@
+helpPatch sequence $dir/sequence.help.jmax
+helpPatch seqstep $dir/seqstep.help.jmax
+helpPatch seqplay $dir/seqplay.help.jmax
+helpPatch seqrec $dir/seqrec.help.jmax
+helpPatch seqfind $dir/seqfind.help.jmax
