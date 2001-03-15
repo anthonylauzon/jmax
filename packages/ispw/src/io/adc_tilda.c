a314cc3d443e68e554c0d73763cd4c357bfa74d4
dechelle <dechelle>
2001-03-15 16:28:23 +0000

    Fixed adc~ (was dead drunk ...)

--- packages/ispw/src/io/adc_tilda.c
+++ packages/ispw/src/io/adc_tilda.c
@@ -31,88 +31,48 @@
 
 #include <fts/fts.h>
 
-typedef struct {
-  fts_object_t head;
-  fts_object_t **targets;
-} adc_tilda_t;
-
 static void adc_tilda_init( fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
-  adc_tilda_t *this = (adc_tilda_t *)o;
   fts_audioport_t *port;
-  int i, inlets;
+  int i, outlets;
 
   ac--;
   at++;
 
-  inlets = fts_object_get_inlets_number( o);
-
-  this->targets = (fts_object_t **)fts_malloc( inlets * sizeof( fts_object_t *));
-
   port = fts_audioport_get_default();
+
+  outlets = fts_object_get_outlets_number( o);
 
   if ( ac != 0)
     {
-      for ( i = 0; i < inlets; i++)
-	this->targets[i] = fts_audioport_get_out_object( port, fts_get_long(at + i) - 1);
+      for ( i = 0; i < outlets; i++)
+	fts_audioport_get_in_object( port, o, fts_get_long(at + i) - 1);
     }
   else
     {
-      this->targets[0] = fts_audioport_get_out_object( port, 0);
-      this->targets[1] = fts_audioport_get_out_object( port, 1);
+      fts_audioport_get_in_object( port, o, 0);
+      fts_audioport_get_in_object( port, o, 1);
     }
-}
-
-static void adc_tilda_delete( fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
-{
-  adc_tilda_t *this = (adc_tilda_t *)o;
-  int i;
-
-  /* For now, we leave the objects that were created
-     Otherwise, it crashes when you quit because it tries to delete
-     objects that were already deleted
-     Anyway, if you delete the adc~ object, the dispatch objects
-     will no longer be visited when you create the dsp chain
-     because of the propagation mechanism
-  */
-
-/*    for ( i = 0; i < fts_object_get_inlets_number( o); i++) */
-/*      fts_object_delete_from_patcher( this->targets[ i]); */
-}
-
-static void adc_tilda_propagate_input(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
-{
-  adc_tilda_t *this  = (adc_tilda_t *)o;
-  fts_propagate_fun_t propagate_fun = (fts_propagate_fun_t)fts_get_fun(at + 0);
-  void *propagate_context = fts_get_ptr(at + 1);
-  int n = fts_get_int(at + 2);
-
-  if ( this->targets)
-    (*propagate_fun)( propagate_context, this->targets[n], 0);
 }
 
 static fts_status_t adc_tilda_instantiate(fts_class_t *cl, int ac, const fts_atom_t *at)
 {
-  int inlets, i;
-  fts_audioport_t *port;
+  int outlets, i;
 
   ac--;
   at++;
 
   if (ac == 0)
-    inlets = 2;
+    outlets = 2;
   else
-    inlets = ac - 1;
+    outlets = ac - 1;
 
-  fts_class_init( cl, sizeof( adc_tilda_t), inlets, 0, 0);
+  fts_class_init( cl, sizeof( fts_object_t), 0, outlets, 0);
 
   fts_method_define_varargs(cl, fts_SystemInlet, fts_s_init, adc_tilda_init);
-  fts_method_define_varargs(cl, fts_SystemInlet, fts_s_delete, adc_tilda_delete);
 
-  for ( i = 0; i < inlets; i++)
-    fts_dsp_declare_inlet( cl, i);
-
-  fts_class_define_thru( cl, adc_tilda_propagate_input);
+  for ( i = 0; i < outlets; i++)
+    fts_dsp_declare_outlet( cl, i);
 
   return fts_Success;
 }
