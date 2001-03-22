d1935862f62f66c0e73c089e0c261f62b97b9f9c
schnell <schnell>
2001-03-22 07:22:17 +0000

    *** empty log message ***

--- packages/unixdtd/help/unixdtd.help.index.tcl
+++ packages/unixdtd/help/unixdtd.help.index.tcl
@@ -1,4 +1,4 @@
-helpPatch readsf~ $dir/readsf~.help.jmax
-helpPatch writesf~ $dir/writesf~.help.jmax
-
 helpSummary "DTD summary" $dir/unixdtd.summary.jmax
+
+helpPatch readsf~ $dir/sf~.help.jmax
+helpPatch writesf~ $dir/sf~.help.jmax
