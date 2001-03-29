a605645281367f105e4fd185a6239618dd2fc587
dechelle <dechelle>
2001-03-29 16:00:46 +0000

    Trying audio on Mac OS X

--- packages/macosx/src/halaudioport.c
+++ packages/macosx/src/halaudioport.c
@@ -29,6 +29,9 @@
  */
 
 #include <stdio.h>
+#include <Carbon/Carbon.h>
+#include <CoreAudio/AudioHardware.h>
+#include <unistd.h>
 
 #include <fts/fts.h>
 
@@ -38,6 +41,11 @@
 
 typedef struct {
   fts_audioport_t head;
+  AudioDeviceID device;
+  int pipe[2];
+  float *buffer;
+  int count;
+  int buffer_size;
 } halaudioport_t;
 
 static void halaudioport_input( fts_word_t *argv)
@@ -56,12 +64,6 @@ static void halaudioport_input( fts_word_t *argv)
       j = ch;
       for ( i = 0; i < n; i++)
 	{
-	  short s0 = port->adc_fmtbuf[j];
-
-#ifdef FTS_HAS_BIG_ENDIAN
-	  SWAP_SHORT( s0);
-#endif
-
 	  out[i] = (float)s0 / 32767.0f;
 	  j += channels;
 	}
@@ -70,36 +72,49 @@ static void halaudioport_input( fts_word_t *argv)
 
 static void halaudioport_output( fts_word_t *argv)
 {
-  halaudioport_t *port;
+  halaudioport_t *this;
   int n, channels, ch, i, j;
 
-  port = (halaudioport_t *)fts_word_get_ptr( argv+0);
+  this = (halaudioport_t *)fts_word_get_ptr( argv+0);
   n = fts_word_get_long(argv + 1);
-  channels = fts_audioport_get_output_channels( port);
-
-  port->bytes_count += (channels * n * sizeof(short));
+  channels = fts_audioport_get_output_channels( (fts_audioport_t *)this);
 
   for ( ch = 0; ch < channels; ch++)
     {
       float *in = (float *) fts_word_get_ptr( argv + 2 + ch);
 
-      j = ch;
+      j = this->count + ch;
       for ( i = 0; i < n; i++)
 	{
-	  short s0 = (short) ( 32767.0f * in[i]);
-
-#ifdef FTS_HAS_BIG_ENDIAN
-	  SWAP_SHORT( s0);
-#endif
-
-	  port->dac_fmtbuf[j] = s0;
+	  this->buffer[j] = in[i];
 	  j += channels;
 	}
     }
 
-  write( port->fd, port->dac_fmtbuf, channels * n * sizeof(short));
+  this->count += n * channels;
+
+  if (port->count >= port->buffer_size)
+    {
+      read( this->pipe[0], &c, 1);
+      this->count = 0;
+    }
 }
 
+OSStatus halaudioport_ioproc( AudioDeviceID inDevice, 
+			      const AudioTimeStamp *inNow, 
+			      const AudioBufferList *inInputData, 
+			      const AudioTimeStamp *inInputTime, 
+			      AudioBufferList *outOutputData,
+			      const AudioTimeStamp *inOutputTime,
+			      void *inClientData)
+{
+  halaudioport_t *this = (halaudioport_t *)inClientData;
+  char c;
+
+  memcpy( outOutputData->mBuffers[0].mData, this->buffer, this->buffer_size);
+
+  write( this->pipe[1], &c, 1);
+}
 
 static int halaudioport_xrun( fts_audioport_t *port)
 {
@@ -110,50 +125,81 @@ static int halaudioport_xrun( fts_audioport_t *port)
 
 static void halaudioport_init( fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
-  int sample_rate, p_sample_rate, fragparam, fragment_size, channels, p_channels, flags, format, i;
-  float sr;
   halaudioport_t *this = (halaudioport_t *)o;
-  char device_name[256];
+  int fifo_size;
+  OSStatus err;
+  UInt32 count;
+  UInt32 bufferSizeProperty;
 
   fts_audioport_init( &this->head);
 
   ac--;
   at++;
-  sr = fts_param_get_float( fts_s_sampling_rate, DEFAULT_SAMPLING_RATE);
-  sample_rate = (int)sr ;
-  this->fifo_size = fts_param_get_int(fts_s_fifo_size, DEFAULT_FIFO_SIZE);
-
-  strcpy( device_name, fts_symbol_name( fts_get_symbol_arg( ac, at, 0, s_slash_dev_slash_audio)));
+  fifo_size = fts_param_get_int( fts_s_fifo_size, DEFAULT_FIFO_SIZE);
 
   channels = fts_get_int_arg( ac, at, 1, DEFAULT_CHANNELS);
 
-  if (flags != O_WRONLY)
+  count = sizeof( this->device);
+  err = AudioHardwareGetProperty( kAudioHardwarePropertyDefaultOutputDevice, &count, (void *)&this->device);
+  if (err != noErr)
     {
-      fts_audioport_set_input_channels( (fts_audioport_t *)this, channels);
-      fts_audioport_set_input_function( (fts_audioport_t *)this, halaudioport_input);
-    }
-  if (flags != O_RDONLY)
-    {
-      fts_audioport_set_output_channels( (fts_audioport_t *)this, channels);
-      fts_audioport_set_output_function( (fts_audioport_t *)this, halaudioport_output);
+      fts_object_set_error( o, "Cannot get default device");
+      return;
     }
 
-  fts_audioport_set_xrun_function( (fts_audioport_t *)this, halaudioport_xrun);
+  count = sizeof( bufferSizeProperty);
+  err = AudioDeviceGetProperty( this->device, 0, false, kAudioDevicePropertyBufferSize, &count, &bufferSizeProperty);
+  if (err != noErr)
+    {
+      fts_object_set_error( o, "Cannot get buffer size");
+      return;
+    }
 
-  this->adc_fmtbuf = (short *)fts_malloc( fts_get_tick_size() * channels * sizeof(short));
-  this->dac_fmtbuf = (short *)fts_malloc( fts_get_tick_size() * channels * sizeof(short));
+  fts_audioport_set_output_channels( (fts_audioport_t *)this, channels);
 
-  this->no_xrun_message_already_posted = 0;
+  fts_audioport_set_output_function( (fts_audioport_t *)this, halaudioport_output);
+
+  this->buffer = (float *)fts_malloc( bufferSizeProperty * sizeof(float));
+  this->buffer_size = bufferSizeProperty;
+
+  if ( pipe( this->pipe) < 0)
+    {
+      fts_object_set_error( o, "Cannot open pipe");
+      return;
+    }
+    
+
+  err = AudioDeviceAddIOProc( this->device, halaudioport_ioproc, this);
+  if (err != noErr)
+    {
+      fts_object_set_error( o, "Cannot set IO proc");
+      return;
+    }
+  
+  err = AudioDeviceStart( p->device, ioproc);
+  if (err != noErr)
+    {
+      fts_object_set_error( o, "Cannot start device");
+      return;
+    }
 }
 
 static void halaudioport_delete(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
   halaudioport_t *this = (halaudioport_t *)o;
+  OSStatus err;
 
   fts_audioport_delete( &this->head);
 
-  fts_free( this->adc_fmtbuf);
-  fts_free( this->dac_fmtbuf);
+  err = AudioDeviceStop( this->device, halaudioport_ioproc);
+  if (err != noErr)
+    ;
+
+  err = AudioDeviceRemoveIOProc( p->device, halaudioport_ioproc);
+  if (err != noErr)
+    ;
+
+  fts_free( this->buffer);
 }
 
 static void halaudioport_get_state( fts_daemon_action_t action, fts_object_t *o, fts_symbol_t property, fts_atom_t *value)
