28529e4e8d1be9c2a7ef067462ce7d79c03862ba
schnell <schnell>
2001-10-04 14:00:25 +0000

    finalized (?) sync class

--- packages/control/c/src/sync.c
+++ packages/control/c/src/sync.c
@@ -27,13 +27,20 @@
 #include <fts/fts.h>
 
 static fts_symbol_t sym_all = 0;
+static fts_symbol_t sym_none = 0;
+
 static fts_symbol_t sym_any = 0;
+static fts_symbol_t sym_left = 0;
+static fts_symbol_t sym_right = 0;
 
 typedef struct 
 {
   fts_object_t o;
   int n;
-  unsigned long bits;
+  unsigned int trigger; /* control bits: trigger on input at given inlets */
+  unsigned int require; /* control bits: require input on given inlets */
+  unsigned int reset; /* control bits: reset memory of given inputs after on each input */
+  unsigned int wait; /* status bits: wait for input at given inlet before output */
   fts_atom_t a[32];
   enum {mode_all, mode_select} mode;
 } sync_t;
@@ -51,7 +58,9 @@ sync_output(sync_t *this)
 
   for(i=this->n-1; i>=0; i--)
     {
-      if(!fts_is_void(this->a + i))
+      if(fts_is_void(this->a + i))
+	fts_outlet_bang((fts_object_t *)this, i);
+      else
 	fts_outlet_send((fts_object_t *)this, i, fts_get_selector(this->a + i), 1, this->a + i);
     }
 }
@@ -60,21 +69,95 @@ static void
 sync_input(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
   sync_t *this = (sync_t *)o;
-
-  fts_atom_assign(this->a + winlet, at);
-
-  if(this->mode == mode_all)
+  
+  if(ac)
     {
-      this->bits &= ~(1 << winlet);
-
-      if(this->bits == 0)
+      unsigned int bit = 1 << winlet;
+      
+      fts_atom_assign(this->a + winlet, at);
+      
+      this->wait &= ~bit;
+      
+      if(!this->wait && (this->trigger & bit))
 	{
 	  sync_output(this);
-	  this->bits = (1 << this->n) - 1;
+	  this->wait |= this->reset & this->require;
 	}
     }
-  else if(this->bits & (1 << winlet))
-    sync_output(this);
+}
+
+static void
+sync_set_bits(unsigned int *bits, int n, const fts_atom_t *at, int sign)
+{
+  if(fts_is_symbol(at))
+    {
+      fts_symbol_t mode = fts_get_symbol(at);
+      
+      if(mode == sym_all)
+	*bits = (1 << n) - 1;
+      else if(mode == sym_none)
+	*bits = 0;
+    }
+  else if(fts_is_number(at))
+    {
+      int in = fts_get_number_int(at) * sign;
+
+      if(in >= 0 && in < n)
+	*bits = 1 << in;
+    }
+  else if(fts_is_list(at))
+    {
+      fts_list_t *l = fts_get_list(at);
+      fts_atom_t *a = fts_list_get_ptr(l);
+      int size = fts_list_get_size(l);
+      int i;
+
+      *bits = 0;
+
+      for(i=0; i<size; i++)
+	{
+	  if(fts_is_number(a + i))
+	    {
+	      int in = fts_get_number_int(a + i) * sign;
+	      
+	      if(in >= 0 && in < n)
+		*bits |= 1 << in;
+	    }  
+	}
+    }
+}
+
+static void
+sync_set_trigger(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+{
+  sync_t *this = (sync_t *)o;
+
+  sync_set_bits(&this->trigger, this->n, at, 1);
+}
+
+static void
+sync_set_trigger_prop(fts_daemon_action_t action, fts_object_t *o, fts_symbol_t property, fts_atom_t *value)
+{
+  sync_set_trigger(o, 0, 0, 1, value);
+}
+
+static void
+sync_set_require(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+{
+  sync_t *this = (sync_t *)o;
+  unsigned int once = 0;
+
+  sync_set_bits(&this->require, this->n, at, 1);
+  sync_set_bits(&once, this->n, at, -1);
+
+  this->reset = ~once;
+  this->wait = this->require | once;
+}
+
+static void
+sync_set_require_prop(fts_daemon_action_t action, fts_object_t *o, fts_symbol_t property, fts_atom_t *value)
+{
+  sync_set_require(o, 0, 0, 1, value);
 }
 
 static void
@@ -86,53 +169,26 @@ sync_set_mode(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_ato
     {
       fts_symbol_t mode = fts_get_symbol(at);
       
-      if(mode == sym_all)
+      if(mode == sym_any)
 	{
-	  this->mode = mode_all;
-	  this->bits = 0;
+	  this->trigger = (1 << this->n) - 1;
+	  this->reset = 0;
+	  this->require = 0;
 	}
-      else if(mode == sym_any)
+      else if(mode == sym_left)
 	{
-	  this->mode = mode_select;
-	  this->bits = 0xffffffff;
+	  this->trigger = 1;
+	  this->reset = 0;
+	  this->require = 0;
 	}
-    }
-  else if(fts_is_number(at))
-    {
-      int in = fts_get_number_int(at);
-
-      if(in < 0)
-	in = 0;
-      else if(in > this->n)
-	in = this->n;
-
-      this->mode = mode_select;
-      this->bits = 1 << in;
-    }
-  else if(fts_is_list(at))
-    {
-      fts_list_t *l = fts_get_list(at);
-      fts_atom_t *a = fts_list_get_ptr(l);
-      int size = fts_list_get_size(l);
-      int i;
-
-      this->mode = mode_select;
-      this->bits = 0;
-
-      for(i=0; i<size; i++)
+      else if(mode == sym_right)
 	{
-	  if(fts_is_number(a + i))
-	    {
-	      int in = fts_get_number_int(a + i);
-	      
-	      if(in < 0)
-		in = 0;
-	      else if(i > this->n)
-		in = this->n;
-	      
-	      this->bits += 1 << in;
-	    }  
+	  this->trigger = (1 << (this->n - 1));
+	  this->reset = 0;
+	  this->require = 0;
 	}
+
+      this->wait = this->require;
     }
 }
 
@@ -184,8 +240,7 @@ sync_init(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t 
     fts_object_set_error(o, "Wrong arguments");
 
   this->n = n;
-  this->bits = (1 << n) - 1;
-  this->mode = mode_all;
+  this->trigger = this->require = this->reset = this->wait = (1 << n) - 1;
 }
 
 static fts_status_t
@@ -208,7 +263,10 @@ sync_instantiate(fts_class_t *cl, int ac, const fts_atom_t *at)
   fts_class_init(cl, sizeof(sync_t), n, n, 0);
   
   fts_method_define_varargs(cl, fts_SystemInlet, fts_s_init, sync_init);
-
+  fts_class_add_daemon(cl, obj_property_put, fts_new_symbol("trigger"), sync_set_trigger_prop);
+  fts_class_add_daemon(cl, obj_property_put, fts_new_symbol("require"), sync_set_require_prop);
+  fts_class_add_daemon(cl, obj_property_put, fts_new_symbol("mode"), sync_set_mode_prop);
+      
   for(i=0; i<n; i++)
     fts_method_define_varargs(cl, i, fts_s_anything, sync_input);
   
@@ -219,7 +277,11 @@ void
 sync_config(void)
 {
   sym_all = fts_new_symbol("all");
+  sym_none = fts_new_symbol("none");
   sym_any = fts_new_symbol("any");
+  sym_left = fts_new_symbol("left");
+  sym_right = fts_new_symbol("right");
 
   fts_metaclass_install(fts_new_symbol("sync"), sync_instantiate, fts_first_arg_equiv);
 }
+
