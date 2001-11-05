f66e7e18c03c1b0ea5075a66aa95abfd15cf4f52
dechelle <dechelle>
2001-11-05 16:23:31 +0000

    Fixing non-portable on Mac OS X

--- fts/src/non-portable/macosx.c
+++ fts/src/non-portable/macosx.c
@@ -33,7 +33,7 @@
 #include <string.h>
 #include <mach-o/dyld.h> 
 
-#include <fts/sys/hw.h>
+#include <fts/sys.h>
 
 
 /* *************************************************************************** */
