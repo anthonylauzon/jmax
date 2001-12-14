50b0af7def6a5fced9cb47f78281ac59282fe87c
schnell <schnell>
2001-12-14 11:22:19 +0000

    *** empty log message ***

--- packages/sequence/c/src/track.c
+++ packages/sequence/c/src/track.c
@@ -542,6 +542,7 @@ void
 track_upload(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
   track_t *this = (track_t *)o;
+  fts_symbol_t type = track_get_type(track);
   event_t *event = track_get_first(this);
   fts_atom_t a[TRACK_BLOCK_SIZE];
   fts_array_t array;
@@ -552,7 +553,7 @@ track_upload(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom
       fts_symbol_t name = track_get_name(this); 
      
       /* create track at client */
-      fts_set_symbol(a, track_get_type(this));
+      fts_set_symbol(a, type);
       fts_client_upload((fts_object_t *)this, seqsym_track, 1, a);
 
       if(name)
@@ -561,6 +562,10 @@ track_upload(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom
 	  fts_client_send_message(o, seqsym_setName, 1, a);
 	}
     }
+
+  /* upload event only for known types */
+  if(type != fts_s_int && type != fts_s_float && type != fts_s_message && type != seqsym_note)
+    return;
   
   fts_array_init(&array, 0, 0);
 
@@ -613,11 +618,14 @@ track_print(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_
 {
   track_t *this = (track_t *)o;
   fts_symbol_t track_name = track_get_name(this);
-  fts_symbol_t track_type = this->type;
+  fts_symbol_t track_type = track_get_type(this);
+  int track_size = track_get_size(this);
   event_t *event = track_get_first(this);  
   fts_array_t array;
   
-  post("track of %s (%s): %d event(s)\n", fts_symbol_name(track_type), track_name? fts_symbol_name(track_name): "untitled", track_get_size(this));
+  post("track of %s: %s (%d event%s)\n", fts_symbol_name(track_type), 
+       track_name? fts_symbol_name(track_name): "untitled", 
+       track_size, (track_size > 1)? "s": "");
   
   fts_array_init(&array, 0, 0);
 
@@ -649,8 +657,68 @@ track_print(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_
  * 
  */
 
+static void
+track_import_midifile(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+{
+  track_t *this = (track_t *)o;
+  fts_symbol_t name = fts_get_symbol(at);
+  fts_midifile_t *file = fts_midifile_open_read(name);
+  
+  if(file)
+    {
+      int size = track_import_from_midifile(this, file);
+      
+      if(fts_midifile_get_error(file))
+	post("track: error reading MIDI file %s (%s)\n", fts_symbol_name(name), fts_midifile_get_error(file));
+      else if(size <= 0)
+	post("track: couldn't import from MIDI file %s\n", fts_midifile_get_name(file));
+      
+      fts_midifile_close(file);
+      
+      if(track_editor_is_open(this))
+	track_upload(o, 0, 0, 0, 0);
+    }
+  else
+    post("track: cannot open file %s\n", fts_symbol_name(name));
+}
+      
+static void
+track_import_midifile_dialog(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+{
+  track_t *this = (track_t *)o;
+  fts_symbol_t track_name = track_get_name(this);
+  fts_symbol_t default_name;
+  char str[1024];
+  fts_atom_t a[4];
+
+  snprintf(str, 1024, "%s.mid", track_name? fts_symbol_name(track_name): "untitled");
+  default_name = fts_new_symbol_copy(str);
+  fts_set_symbol(a, seqsym_import_midifile);
+  fts_set_symbol(a + 1, fts_new_symbol("Open standard MIDI file"));
+  fts_set_symbol(a + 2, fts_project_get_dir());
+  fts_set_symbol(a + 3, default_name);
+
+  if(track_editor_is_open(this))
+    fts_client_send_message((fts_object_t *)this, seqsym_openFileDialog, 4, a);
+  else if(track_get_sequence(this))
+    fts_client_send_message((fts_object_t *)track_get_sequence(this), seqsym_openFileDialog, 4, a);    
+}
+
+static void
+track_import(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+{
+  track_t *this = (track_t *)o;
+
+  if(ac == 0)
+    track_import_midifile_dialog(o, 0, 0, 0, 0);
+  else if(ac == 1 && fts_is_symbol(at))
+    track_import_midifile(o, 0, 0, 1, at);
+  else
+    fts_object_signal_runtime_error(o, "import: wrong arguments");  
+}
+
 static void 
-track_export_to_midifile(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+track_export_midifile(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
   track_t *this = (track_t *)o;
   fts_symbol_t name = fts_get_symbol(at);
@@ -658,7 +726,7 @@ track_export_to_midifile(fts_object_t *o, int winlet, fts_symbol_t s, int ac, co
 
   if(file)
     {
-      int size = track_export_midifile(this, file);
+      int size = track_export_to_midifile(this, file);
 
       if(fts_midifile_get_error(file))
 	post("track: error writing MIDI file %s (%s)\n", file->name, (file->error)? file->error: "unknown");
@@ -672,7 +740,7 @@ track_export_to_midifile(fts_object_t *o, int winlet, fts_symbol_t s, int ac, co
 }
 
 static void 
-track_export_to_midifile_dialog(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+track_export_midifile_dialog(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
   track_t *this = (track_t *)o;
   fts_symbol_t track_name = track_get_name(this);
@@ -686,7 +754,24 @@ track_export_to_midifile_dialog(fts_object_t *o, int winlet, fts_symbol_t s, int
   fts_set_symbol(a + 1, fts_new_symbol("Save standard MIDI file"));
   fts_set_symbol(a + 2, fts_project_get_dir());
   fts_set_symbol(a + 3, default_name);
-  fts_client_send_message((fts_object_t *)this, seqsym_openFileDialog, 4, a);
+
+  if(track_editor_is_open(this))
+    fts_client_send_message((fts_object_t *)this, seqsym_openFileDialog, 4, a);
+  else if(track_get_sequence(this))
+    fts_client_send_message((fts_object_t *)track_get_sequence(this), seqsym_openFileDialog, 4, a);    
+}
+
+static void
+track_export(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+{
+  track_t *this = (track_t *)o;
+
+  if(ac == 0)
+    track_export_midifile_dialog(o, 0, 0, 0, 0);
+  else if(ac == 1 && fts_is_symbol(at))
+    track_export_midifile(o, 0, 0, 1, at);
+  else
+    fts_object_signal_runtime_error(o, "export: wrong arguments");  
 }
 
 /******************************************************
@@ -847,9 +932,14 @@ track_instantiate(fts_class_t *cl, int ac, const fts_atom_t *at)
   fts_method_define_varargs(cl, fts_SystemInlet, fts_s_print, track_print);
   fts_method_define_varargs(cl, fts_SystemInlet, fts_s_clear, track_clear_method);
 
-  fts_method_define_varargs(cl, fts_SystemInlet, seqsym_export_midifile_dialog, track_export_to_midifile_dialog);
-  fts_method_define_varargs(cl, fts_SystemInlet, seqsym_export_midifile, track_export_to_midifile);
-    
+  fts_method_define_varargs(cl, fts_SystemInlet, seqsym_import_midifile_dialog, track_import_midifile_dialog);
+  fts_method_define_varargs(cl, fts_SystemInlet, seqsym_import_midifile, track_import_midifile);
+  fts_method_define_varargs(cl, fts_SystemInlet, fts_s_import, track_import);
+
+  fts_method_define_varargs(cl, fts_SystemInlet, seqsym_export_midifile_dialog, track_export_midifile_dialog);
+  fts_method_define_varargs(cl, fts_SystemInlet, seqsym_export_midifile, track_export_midifile);
+  fts_method_define_varargs(cl, fts_SystemInlet, fts_s_export, track_export);
+
   fts_method_define_varargs(cl, fts_SystemInlet, seqsym_setName, track_set_name_by_client_request);
   fts_method_define_varargs(cl, fts_SystemInlet, seqsym_addEvent, track_add_event_by_client_request);
   fts_method_define_varargs(cl, fts_SystemInlet, seqsym_makeEvent, track_make_event_by_client_request);
