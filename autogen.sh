ed9c641c00330966861b591bae2009ad04e034d3
dechelle <dechelle>
2001-12-21 13:11:41 +0000

    Debugging 'make dist' and RPM building

--- autogen.sh
+++ autogen.sh
@@ -3,11 +3,12 @@ set -x
 aclocal -I m4
 libtoolize --automake --copy
 autoconf
-automake --foreign -v --add-missing --copy
+automake --foreign --add-missing --copy
 (
     cd client
     aclocal
     autoconf
+    automake --foreign --add-missing --copy
 )
 
 
