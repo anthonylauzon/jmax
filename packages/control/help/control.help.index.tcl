cf04faaf944411005fd74fcd3a649b41b54c860a
schnell <schnell>
2002-04-03 10:19:09 +0000

    deleted .tcl files in all packages

--- packages/control/help/control.help.index.tcl
+++
@@ -1,6 +0,0 @@
-helpPatch switch $dir/switch.help.jmax
-helpPatch oneshot $dir/oneshot.help.jmax
-helpPatch demux $dir/demux.help.jmax
-helpPatch for $dir/for.help.jmax
-helpPatch range $dir/range.help.jmax
-helpPatch count $dir/count.help.jmax
