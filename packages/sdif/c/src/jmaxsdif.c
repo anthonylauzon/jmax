b96e66fa272afafb1eac79d8373d1e1743a62902
schwarz <schwarz>
2002-09-02 10:39:14 +0000

    Working list i/o. Version output included.

--- packages/sdif/c/src/jmaxsdif.c
+++ packages/sdif/c/src/jmaxsdif.c
@@ -1,3 +1,32 @@
+/* $Id$
+ *
+ * jMax
+ * Copyright (C) 1994-2002 by IRCAM-Centre Georges Pompidou, Paris, France.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * See file LICENSE for further informations on licensing terms.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ */
+
+/*
+ * This file's authors:  Diemo Schwarz, 20.8.2002
+ */
+
+/* 
+ * $Log$
+ */
 
 
 #include "jmaxsdif.h"
@@ -53,7 +82,7 @@ void sdif_config(void)
     s_record = fts_new_symbol("record");
     s_close  = fts_new_symbol("close");
 
-    post("sdif package built %s\n", BUILD_DATE);
+    post("sdif package version %s built %s\n", JMAXSDIFVERSION, BUILD_DATE);
 }
 
 
--- packages/sdif/c/src/jmaxsdif.h
+++ packages/sdif/c/src/jmaxsdif.h
@@ -1,3 +1,37 @@
+/* $Id$
+ *
+ * jMax
+ * Copyright (C) 1994-2002 by IRCAM-Centre Georges Pompidou, Paris, France.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * See file LICENSE for further informations on licensing terms.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/*
+ * This file's authors:  Diemo Schwarz, 20.8.2002
+ */
+
+/* 
+ * $Log$
+ */
+
+
+#ifndef _JMAXSDIF_H_
+#define _JMAXSDIF_H_
+
 
 #include <fts/fts.h>
 #include <sdif.h>
@@ -11,7 +45,7 @@
 #   define jmaxsdif_get_time()	fts_timebase_get_time(timebase)
 #endif
 
-#define JMAXSDIFVERSION "0_1"
+#define JMAXSDIFVERSION "0_2"
 
 
 typedef enum { state_stopped, state_running } jmaxsdif_state_t;
@@ -51,3 +85,6 @@ int  jmaxsdif_closefile(jmaxsdif_t *this);
 
 fts_status_t readsdif_instantiate(fts_class_t *, int, const fts_atom_t *);
 fts_status_t writesdif_instantiate(fts_class_t *, int, const fts_atom_t *);
+
+
+#endif /* ifndef _JMAXSDIF_H_ */
