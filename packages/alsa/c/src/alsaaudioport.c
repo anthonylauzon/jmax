040a15efaee2d5e3ff8bbf8f9ddcff8118c29f54
tisseran <tisseran>
2002-09-06 08:33:55 +0000

    TEMPORARY FIX: Change default access type. Actually we need to use rw_interleaved to use .asoundrc "plugins" Since we have change DEFAULT_PCM_NAME from "hw:0,0" to "default", by default "default" pcm is a plugin but it also support mmap_interleaved (with no sound on my Hammerfall DSP Multiface).

--- packages/alsa/c/src/alsaaudioport.c
+++ packages/alsa/c/src/alsaaudioport.c
@@ -56,7 +56,7 @@ static fts_symbol_t s_mmap_noninterleaved, s_mmap_interleaved, s_rw_noninterleav
    we check which parameters are supported by audio card.
 */
 #define DEFAULT_FORMAT s_s32_le
-#define DEFAULT_ACCESS s_mmap_noninterleaved
+#define DEFAULT_ACCESS s_rw_interleaved
 #define DEFAULT_SAMPLING_RATE (44100.)
 #define DEFAULT_FIFO_SIZE 8192
 #define DEFAULT_INPUT_CHANNELS 26
