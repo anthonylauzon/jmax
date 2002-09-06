b855d494598533dd2dd3f542fc7919ad1a0bb813
borghesi <borghesi>
2002-09-06 14:17:52 +0000

    *** empty log message ***

---
+++ macosxbuild/jMaxProject/JMaxWrapper.java
@@ -0,0 +1,17 @@
+//
+//  JMaxWrapper.java
+//  jMaxProject
+//
+//  Created by riccardo borghesi on Fri Sep 06 2002.
+//  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
+//
+
+import java.awt.*;
+import ircam.jmax.JMaxApplication;
+
+public class JMaxWrapper {
+ public static void main( String args[]) 
+  {
+    JMaxApplication.main( args);
+  }
+}
