7df77e094bfea3ce7585580437da432a8af31edd
tisseran <tisseran>
2002-09-17 19:12:41 +0000

    ntegration of threaded direct to disk (package dtd) First integration of the fts_thread_manager (TM). The TM is a running thread which wait for creating thread. (see fts/thred.c and include/fts/thread.h) TODO: - Cancel thread in thread manager thread and not in fts thread. - Properly stop thread manager

--- include/fts/thread.h
+++ include/fts/thread.h
@@ -1,5 +1,4 @@
 #include <pthread.h>
-#include <fts/fts.h>
 
 /**************************************************
  *
@@ -10,7 +9,16 @@
 /**
  * Thread job function
  */
-typedef void* (*fts_thread_function_t)(void*);
+typedef struct fts_thread_function
+{
+    fts_object_t* object;
+    fts_method_t method;
+    fts_symbol_t symbol;
+    int ac;
+    fts_atom_t* at;
+    int delay_ms; /* sleep delay in millisecond */
+
+} fts_thread_function_t;
 
 
 /**
@@ -18,20 +26,31 @@ typedef void* (*fts_thread_function_t)(void*);
  */
 typedef struct fts_thread_worker
 {
-    fts_fifo_t fifo;
     pthread_t thread;
-    fts_thread_function_t thread_function;
+    fts_thread_function_t* thread_function;
+    int id;
 } fts_thread_worker_t;
 
 /** 
  * Create a thread with the given 
  */
-FTS_API int fts_thread_manager_create(fts_thread_worker_t* thread_worker,
-				      void* arg,
-				      int* thread_id);
+FTS_API int fts_thread_manager_create_thread(fts_thread_worker_t* thread_worker);
 
 /**
  * Stop this thread
  * Need a thread id
  */
-FTS_API int fts_thread_manager_cancel(fts_thread_worker_t* thread_worker);
+FTS_API int fts_thread_manager_cancel_thread(fts_thread_worker_t* thread_worker);
+
+/** 
+ * @func fts_thread_mananger_start
+ * Start the fts_thread_manager 
+ * 
+ * 
+ * @return 0 if a fts_manager is running 
+ * @return -1 if an error occured
+ */
+
+FTS_API int fts_thread_manager_start(void);
+
+
