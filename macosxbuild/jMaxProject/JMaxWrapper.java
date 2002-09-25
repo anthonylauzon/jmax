7a4e80484e7e4f5670513734c256d34dc37b81ea
borghesi <borghesi>
2002-09-25 15:21:15 +0000

    *** empty log message ***

--- macosxbuild/jMaxProject/JMaxWrapper.java
+++ macosxbuild/jMaxProject/JMaxWrapper.java
@@ -7,15 +7,20 @@
 //
 
 import java.awt.*;
+import java.io.*;
 import com.apple.mrj.*;
-import ircam.jmax.JMaxApplication;
+import ircam.jmax.*;
 
 public class JMaxWrapper {
 
+ static File toOpenFile = null;
  public static void main( String args[]) 
   {
-    initMacOSXApplication();
+    initMacOSXApplication();    
     JMaxApplication.main( args);
+    
+    if( toOpenFile != null)
+        openFile( toOpenFile);
   }
   
  public static void initMacOSXApplication()
@@ -26,5 +31,28 @@ public class JMaxWrapper {
             throw new IllegalStateException("Quit Pending User Confirmation");
         }	
     });
+    MRJApplicationUtils.registerOpenDocumentHandler( new MRJOpenDocumentHandler(){
+        public void handleOpenFile( File file)
+        {
+            if( JMaxApplication.getFtsServer() == null)
+                toOpenFile = file;
+            else
+                openFile( file);
+        }
+    });
+  }
+  
+  public static void openFile( File file)
+  {
+    RecentFileHistory recentFileHistory = JMaxApplication.getRecentFileHistory();
+    recentFileHistory.addFile( file);
+    try
+    {	
+        JMaxApplication.load( file.getAbsolutePath());
+    }
+    catch(IOException e)
+    {
+        System.err.println("[OpenAction]: I/O error loading file "+file.getAbsolutePath());
+    }
   }
 }
