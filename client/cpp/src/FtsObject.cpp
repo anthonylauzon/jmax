4010bf8314e4ec17a354e23f56d2613089ddae52
p_tisserand <p_tisserand>
2002-11-05 10:05:25 +0000

    Implement missing methods (code based on java code)

--- client/cpp/src/FtsObject.cpp
+++ client/cpp/src/FtsObject.cpp
@@ -38,6 +38,114 @@ namespace client {
     return reinterpret_cast<int>(m._ftsClass)  + reinterpret_cast<int>(m._selector);
   }
 
+    FtsObject::FtsObject(FtsServer* server, FtsObject* parent, FtsSymbol* ftsClassName) throw(FtsClientException)
+    {
+	_server = server;
+	_parent = parent;
+	_encoder = server->getEncoder();
+	_id = server->getNewObjectID();
+	server->putObject(_id, this);
+
+	_encoder->writeObject(FtsServer.CLIENT_OBJECT_ID);
+	_encoder->writeSymbol(sNewObject);
+
+	_encoder->writeObject(parent);
+	_encoder->writeInt(_id);
+	_encoder->writeSymbol(ftsClassName);
+	_encoder->flush();
+
+    }
+
+    FtsObject::FtsObject(FtsServer* server, FtsObject* parent, FtsSymbol* ftsClassName, FtsArgs* args) throw(FtsClientException)
+    {
+	_server = server;
+	_parent = parent;
+	_encoder = server->getEncoder();
+	
+	_id = server->getNewObjectID();
+	server->putObject(_id, this);
+
+	_encoder->writeObject(FtsServer.CLIENT_OBJECT_ID);
+	_encoder->writeSymbol(sNewObject);
+	_encoder->writeObject(parent);
+	_encoder->writeInt(_id);
+	_encoder->writeSymbol(ftsClassName);
+	_encoder->writeArgs(args);
+	_encoder->flush();
+    }
+
+    FtsObject::FtsObject(FtsServer* server, FtsObject* parent, int id)
+    {
+	_server = server;
+	_parent = parent;
+	
+	_encoder = server->getEncoder();
+	
+	_id = id;
+	if (_id != NO_ID)
+	{
+	    server->putObject(_id, this);
+	}
+    }
+
+    void FtsObject::send(FtsSymbol* selector, FtsArgs* args) throw(FtsClientException)
+    {
+	_encoder->writeObject(this);
+	_encoder->writeSymbol(selector);
+	_encoder->writeArgs(args);
+	_encoder->flush();
+    }
+
+    void FtsObject::send(FtsSymbol* selector) throw(FtsClientException)
+    {
+	_encoder->writeObject(this);
+	_encoder->writeSymbol(selector);
+	_encoder->flush();
+    }
+
+    void FtsObject::send(FtsArgs* args) throw(FtsClientException)
+    {
+	_encoder->writeObject(this);
+	_encoder->writeSymbol(sList);
+	_encoder->writeArgs(args);
+	_encoder->flush();
+    }
+
+    void FtsObject::send(int n) throw(FtsClientException)
+    {
+	_encoder->writeObject(this);
+	_encoder->writeSymbol(sInt);
+	_encoder->writeInt(n);
+	_encoder->flush();
+    }
+
+    void FtsObject::send(double d) throw(FtsClientException)
+    {
+	_encoder->writeObject(this);
+	_encoder->writeSymbol(sFloat);
+	_encoder->writeDouble(d);
+	_encoder->flush();
+    }
+
+    void FtsObject::sendProperty(FtsArgs* args) throw(FtsClientException)
+    {
+	_encoder->writeObject(FtsServer.CLIENT_OBJECT_ID);
+	_encoder->writeSymbol(FtsSymbol.get("set_object_property"));
+	_encoder->writeObject(this);
+	_encoder->writeArgs(args);
+	_encoder->flush();
+
+    }
+
+    void FtsObject::registerMessageHandler(FtsClass* ftsClass, FtsSymbol* selector, FtsMessageHandler* messageHandler)
+    {
+	if (0 == selector)
+	{
+	    // throw an exception here
+	}
+
+	messageHandlersTable->put(new MessageHandlerEntry(ftsClass, selector), messageHandler);
+    }
 };
 };
 };
