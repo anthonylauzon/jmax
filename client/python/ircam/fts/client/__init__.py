f1648761faf989f9f2087f40073466bfd88c0221
cvs-fast-export <cvs-fast-export>
2002-12-09 14:58:17 +0000

    Synthetic commit for incomplete tag V4_0_1_noaudio_on_ftp

--- client/python/ircam/fts/client/__init__.py
+++
@@ -1,6 +0,0 @@
-# Dummy file for module
-from BinaryProtocolDecoder import BinaryProtocolDecoder
-from FtsServerConnection import FtsServerConnection
-from FtsSocketConnection import FtsSocketConnection
-from FtsObject import FtsObject
-from FtsArgs import FtsArgs
