e611cdaeb1a99325f83eaa9fcee0be9328b1ad1a
p_tisserand <p_tisserand>
2002-12-17 17:31:07 +0000

    Begin to update ossaudioport to be conform with new audio configuration

--- packages/oss/c/src/ossaudioport.c
+++ packages/oss/c/src/ossaudioport.c
@@ -50,10 +50,12 @@
 
 typedef struct {
   fts_audioport_t head;
+  fts_symbol_t device_name;
   int fd;
   int fifo_size;
   short *dac_fmtbuf;
   short *adc_fmtbuf;
+  int buff_size;
   /* output bytes count, for dac slip detection */
   long bytes_count;
   int no_xrun_message_already_posted;
@@ -63,86 +65,89 @@ static fts_symbol_t s_slash_dev_slash_audio;
 static fts_symbol_t s_read_only;
 static fts_symbol_t s_write_only;
 
-static void ossaudioport_input( fts_word_t *argv)
+
+/**
+ * The audioport IO function calls the native audio layer to read/write a buffer
+ * of samples in the native format.
+ * Its argument is the audioport. Buffers are allocated by the port.
+ */
+static void ossaudioport_input_fun(fts_audioport_t* port)
 {
-  ossaudioport_t *port;
-  int n, channels, ch, i, j;
-
-  port = (ossaudioport_t *)fts_word_get_pointer( argv+0);
-  n = fts_word_get_int(argv + 1);
-  channels = fts_audioport_get_input_channels( port);
-
-  read( port->fd, port->adc_fmtbuf, channels * n * sizeof( short));
-
-  for ( ch = 0; ch < channels; ch++)
-    {
-      float *out = (float *) fts_word_get_pointer( argv + 2 + ch);
-
-      j = ch;
-      for ( i = 0; i < n; i++)
-	{
-	  short s0 = port->adc_fmtbuf[j];
-
-	  out[i] = (float)s0 / 32767.0f;
-	  j += channels;
-	}
-    }
+  ossaudioport_t* oss_port = (ossaudioport_t*)port;
+  
+  read(oss_port->fd, oss_port->adc_fmtbuf, oss_port->buff_size);
 }
 
-static void ossaudioport_output( fts_word_t *argv)
+
+static void ossaudioport_output_fun(fts_audioport_t* port)
 {
-  ossaudioport_t *port;
-  int n, channels, ch, i, j;
+  ossaudioport_t* oss_port = (ossaudioport_t*)port;
 
-  port = (ossaudioport_t *)fts_word_get_pointer( argv+0);
-  n = fts_word_get_int(argv + 1);
-  channels = fts_audioport_get_output_channels( port);
-
-  port->bytes_count += (channels * n * sizeof(short));
-
-  for ( ch = 0; ch < channels; ch++)
-    {
-      float *in = (float *) fts_word_get_pointer( argv + 2 + ch);
-
-      j = ch;
-      for ( i = 0; i < n; i++)
-	{
-	  short s0 = (short) ( 32767.0f * in[i]);
-
-	  port->dac_fmtbuf[j] = s0;
-	  j += channels;
-	}
-    }
-
-  write( port->fd, port->dac_fmtbuf, channels * n * sizeof(short));
+  write(oss_port->fd, oss_port->adc_fmtbuf, oss_port->buff_size);
 }
+
+/** 
+ * The audioport copy function copies the samples in the native format to a float buffer
+ * for a given channel.
+ */
+static void ossaudioport_input_copy_fun( fts_audioport_t *port, float *buff, int buffsize, int channel)
+{
+  ossaudioport_t* oss_port = (ossaudioport_t*)port;
+  int channels = fts_audioport_get_max_channels( port, FTS_AUDIO_INPUT);
+  int i;
+  int j = channel;
+
+  for (i = 0; i < buffsize; ++i)
+  {
+    short s0 = oss_port->adc_fmtbuf[j];
+    buff[i] = (float)s0/32767.0f;
+    j += channels;
+  }
+}
+
+static void ossaudioport_output_copy_fun( fts_audioport_t *port, float *buff, int buffsize, int channel)
+{
+  ossaudioport_t* oss_port = (ossaudioport_t*)port;
+  int channels = fts_audioport_get_max_channels( port, FTS_AUDIO_OUTPUT);
+  int i;
+  int j = channel;
+
+  for (i = 0; i < buffsize; ++i)
+  {
+    short s0 = (short)(32767.0f * buff[i]);
+    oss_port->dac_fmtbuf[j] = s0;
+    j += channels;
+  }
+
+}
+
 
 
 static int ossaudioport_xrun( fts_audioport_t *port)
 {
-  ossaudioport_t *ossport = (ossaudioport_t *)port;
-  count_info count;
-  int size;
+/*   ossaudioport_t *ossport = (ossaudioport_t *)port; */
+/*   count_info count; */
+/*   int size; */
   
-  if (ioctl( ossport->fd, SNDCTL_DSP_GETOPTR, &count) < 0)
-    {
-      if ( !ossport->no_xrun_message_already_posted)
-	{
-	  post( "Warning: this device does not support SNDCTL_DSP_GETOPTR\n");
-	  post( "         Synchronisation errors (\"dac slip\") will not be reported\n");
+/*   if (ioctl( ossport->fd, SNDCTL_DSP_GETOPTR, &count) < 0) */
+/*     { */
+/*       if ( !ossport->no_xrun_message_already_posted) */
+/* 	{ */
+/* 	  post( "Warning: this device does not support SNDCTL_DSP_GETOPTR\n"); */
+/* 	  post( "         Synchronisation errors (\"dac slip\") will not be reported\n"); */
 
-	  ossport->no_xrun_message_already_posted = 1;
-	}
+/* 	  ossport->no_xrun_message_already_posted = 1; */
+/* 	} */
 
-      return 0;
-    }
+/*       return 0; */
+/*     } */
 
-  size = fts_audioport_get_output_channels( port) * sizeof( short) * ossport->fifo_size;
-  if (count.bytes > (ossport->bytes_count + size))
-    {
-      ossport->bytes_count = count.bytes;
-      return 1;
-    }
+/*   size = fts_audioport_get_output_channels( port) * sizeof( short) * ossport->fifo_size; */
+/*   if (count.bytes > (ossport->bytes_count + size)) */
+/*     { */
+/*       ossport->bytes_count = count.bytes; */
+/*       return 1; */
+/*     } */
 
   return 0;
 }
@@ -161,18 +166,17 @@ static void ossaudioport_debug( int fd)
 }
 #endif
 
-static void ossaudioport_init( fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+static void ossaudioport_open( fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
+  ossaudioport_t* self = (ossaudioport_t*)o;
   int sample_rate, p_sample_rate, fragparam, fragment_size, channels, p_channels, flags, format, wanted_format, i;
   float sr;
-  ossaudioport_t *this = (ossaudioport_t *)o;
   char device_name[256];
 
-  fts_audioport_init( &this->head);
 
   sr = fts_dsp_get_sample_rate();
   sample_rate = (int)sr ;
-  this->fifo_size = DEFAULT_FIFO_SIZE;
+
 
   strcpy( device_name, fts_get_symbol_arg( ac, at, 0, s_slash_dev_slash_audio));
 
@@ -185,7 +189,7 @@ static void ossaudioport_init( fts_object_t *o, int winlet, fts_symbol_t s, int 
   else
     flags = O_RDWR;
 
-  if ( (this->fd = open( device_name, flags, 0)) < 0 )
+  if ( (self->fd = open( device_name, flags, 0)) < 0 )
     {
       fts_object_set_error( o, "cannot open device \"%s\" (%s)", device_name, strerror( errno));
       post("ossaudioport: cannot open device \"%s\" (%s)", device_name, strerror( errno));
@@ -199,9 +203,9 @@ static void ossaudioport_init( fts_object_t *o, int winlet, fts_symbol_t s, int 
     if (fragment_size & (1<<i))
       break;
 
-  fragparam = ((this->fifo_size / fts_dsp_get_tick_size()) <<16) | (i);
+  fragparam = ((self->fifo_size / fts_dsp_get_tick_size()) <<16) | (i);
 
-  if (ioctl( this->fd, SNDCTL_DSP_SETFRAGMENT, &fragparam))
+  if (ioctl( self->fd, SNDCTL_DSP_SETFRAGMENT, &fragparam))
     {
       fts_object_set_error( o, "cannot set fragment size or number of fragment (%s)", strerror( errno));
       post("ossaudioport: cannot set fragment size or number of fragment (%s)\n", strerror( errno));
@@ -209,7 +213,7 @@ static void ossaudioport_init( fts_object_t *o, int winlet, fts_symbol_t s, int 
     }
 
 #ifdef DEBUG
-  ossaudioport_debug( this->fd);
+  ossaudioport_debug( self->fd);
 #endif
 
   /* Set 16 bit format */
@@ -220,7 +224,7 @@ static void ossaudioport_init( fts_object_t *o, int winlet, fts_symbol_t s, int 
 #endif
 
   wanted_format = format;
-  if ( (ioctl( this->fd, SNDCTL_DSP_SETFMT, &format) == -1) || (format != wanted_format))
+  if ( (ioctl( self->fd, SNDCTL_DSP_SETFMT, &format) == -1) || (format != wanted_format))
     {
       fts_object_set_error( o, "cannot set sample format to signed 16 bits little endian (%s)", strerror( errno));
       post("ossaudioport: cannot set sample format to signed 16 bits little endian (%s)\n", strerror( errno));
@@ -229,7 +233,7 @@ static void ossaudioport_init( fts_object_t *o, int winlet, fts_symbol_t s, int 
 
   /* Set number of channels */
   p_channels = channels;
-  if ( (ioctl( this->fd, SNDCTL_DSP_CHANNELS, &p_channels) == -1) || (channels != p_channels) )
+  if ( (ioctl( self->fd, SNDCTL_DSP_CHANNELS, &p_channels) == -1) || (channels != p_channels) )
     {
       fts_object_set_error( o, "cannot set number of channels (%s)", strerror( errno));
       post("ossaudioport: cannot set number of channels (%s)\n", strerror( errno));
@@ -238,7 +242,7 @@ static void ossaudioport_init( fts_object_t *o, int winlet, fts_symbol_t s, int 
 
   /* Set sample rate */
   p_sample_rate = sample_rate;
-  if (ioctl( this->fd, SNDCTL_DSP_SPEED, &p_sample_rate) == -1)
+  if (ioctl( self->fd, SNDCTL_DSP_SPEED, &p_sample_rate) == -1)
     {
       fts_object_set_error( o, "cannot set sample rate (%s)", strerror( errno));
       post("ossaudioport: cannot set sample rate (%s)\n", strerror( errno));
@@ -252,27 +256,42 @@ static void ossaudioport_init( fts_object_t *o, int winlet, fts_symbol_t s, int 
      fts_log("[ossaudioport]: cannot set to wanted sample rate (%d), get (%d)\n",
 	     sample_rate, p_sample_rate);
  }
+  
+}
 
- /*
-   TODO:
-   Add a conditionnal to know if difference of sampling rate is too important:
-   e.g. a threshold on difference between given and wanted
- */
-  if (flags != O_WRONLY)
-    {
-      fts_audioport_set_input_channels( (fts_audioport_t *)this, channels);
-      fts_audioport_set_input_function( (fts_audioport_t *)this, ossaudioport_input);
-    }
-  if (flags != O_RDONLY)
-    {
-      fts_audioport_set_output_channels( (fts_audioport_t *)this, channels);
-      fts_audioport_set_output_function( (fts_audioport_t *)this, ossaudioport_output);
-    }
+static void ossaudioport_close(fts_object_t* o, int winlet, fts_symbol_t s, int ac, const fts_atom_t* at)
+{
+  ossaudioport_t* self = (ossaudioport_t*)o;
 
-  fts_audioport_set_xrun_function( (fts_audioport_t *)this, ossaudioport_xrun);
+  if (self->fd >= 0)
+    close( self->fd);
 
-  this->adc_fmtbuf = (short *)fts_malloc( fts_dsp_get_tick_size() * channels * sizeof(short));
-  this->dac_fmtbuf = (short *)fts_malloc( fts_dsp_get_tick_size() * channels * sizeof(short));
+}
+
+static void ossaudioport_init( fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+{
+  ossaudioport_t *this = (ossaudioport_t *)o;
+
+  int channels = 2;
+  
+  /* channels is given by audiomanager */
+  fts_audioport_init( &this->head);
+
+  this->fifo_size = DEFAULT_FIFO_SIZE;
+
+  this->device_name = fts_get_symbol(at);
+
+  fts_audioport_set_io_fun((fts_audioport_t*)this, FTS_AUDIO_INPUT, ossaudioport_input_fun);
+  fts_audioport_set_io_fun((fts_audioport_t*)this, FTS_AUDIO_OUTPUT, ossaudioport_output_fun);
+
+  fts_audioport_set_copy_fun((fts_audioport_t*)this, FTS_AUDIO_INPUT, ossaudioport_input_copy_fun);
+  fts_audioport_set_copy_fun((fts_audioport_t*)this, FTS_AUDIO_OUTPUT, ossaudioport_output_copy_fun);
+
+/*   fts_audioport_set_xrun_function( (fts_audioport_t *)this, ossaudioport_xrun); */
+
+  this->buff_size =  fts_dsp_get_tick_size() * channels * sizeof(short);
+  this->adc_fmtbuf = (short *)fts_malloc(this->buff_size);
+  this->dac_fmtbuf = (short *)fts_malloc(this->buff_size);
 
   this->no_xrun_message_already_posted = 0;
 }
@@ -283,8 +302,6 @@ static void ossaudioport_delete(fts_object_t *o, int winlet, fts_symbol_t s, int
 
   fts_audioport_delete( &this->head);
 
-  if (this->fd >= 0)
-    close( this->fd);
 
   fts_free( this->adc_fmtbuf);
   fts_free( this->dac_fmtbuf);
@@ -310,6 +327,13 @@ void ossaudioport_config( void)
    * If ALSA package has already installed a default,
    * we don't overwrite it
    */
-  if (!fts_audioport_get_default_class())
-    fts_audioport_set_default_class(s);
+/*   if (!fts_audioport_get_default_class()) */
+/*     fts_audioport_set_default_class(s); */
 }
+
+/** EMACS **
+ * Local variables:
+ * mode: c
+ * c-basic-offset:2
+ * End:
+ */
