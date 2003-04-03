8b4e0c565bce01e803f81ca15c7c30f876b65f75
p_tisserand <p_tisserand>
2003-04-03 18:30:45 +0000

    Added fts_s_none ("none") and fts_s_noaudio ("noaudio") symbols

--- include/fts/predefsymbols.h
+++ include/fts/predefsymbols.h
@@ -259,4 +259,6 @@ PREDEF_SYMBOL( fts_s_args, "args")
 
 /* Default filename */
 PREDEF_SYMBOL( fts_s_default_project, "default_project.jprj")
-PREDEF_SYMBOL( fts_s_default_config, "default_config.jcfg")
+PREDEF_SYMBOL( fts_s_default_config, "default_config.jcfg")        
+PREDEF_SYMBOL( fts_s_noaudio, "noaudio")    
+PREDEF_SYMBOL( fts_s_none, "none")
