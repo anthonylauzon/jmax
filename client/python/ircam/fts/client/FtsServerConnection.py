23f855e2655f357513b0a672d810926917efa87c
p_tisserand <p_tisserand>
2003-05-13 07:40:31 +0000

    Removed writeAtoms method Added documentation

--- client/python/ircam/fts/client/FtsServerConnection.py
+++ client/python/ircam/fts/client/FtsServerConnection.py
@@ -5,6 +5,9 @@ import BinaryProtocolDecoder
 from FtsClientException import FtsClientException
 
 class FtsServerConnection:
+    """
+    Abstract class for client-server connection
+    """
     DEFAULT_RECEIVE_BUFFER_SIZE = 65536
     CLIENT_OBJECT_ID = 1
     
@@ -17,18 +20,30 @@ class FtsServerConnection:
         raise NotImplementedError
 
     def startThread(self):
+        """
+        Start receive thread
+        """
         self.runningThread = 1
         self.readThread.start()
 
     def stopThread(self):
+        """
+        Stop receive thread
+        """
         self.runningThread = 0
         
     def getNewObjectID(self):
+        """
+        Return a new object ID
+        """
         id = self.__newObjectID
         self.__newObjectID += 2
         return id
     
     def getObject(self, id):
+        """
+        Return object form an ID
+        """
         if self.__objectTable.has_key(id):
             obj = self.__objectTable[id]
         else:
@@ -36,6 +51,9 @@ class FtsServerConnection:
         return obj
     
     def putObject(self,id, obj):
+        """
+        Add an object in objectTable
+        """
         import FtsObject
         if id == FtsObject.FtsObject.NO_ID:
             return FtsObject.FtsObject.NO_ID
@@ -47,53 +65,86 @@ class FtsServerConnection:
         return id
     
     def writeInt(self, v):
+        """
+        Add an int in protocol encoder buffer
+        """
         self.__encoder.writeInt(v)
         return
     
     def writeDouble(self, v):
+        """
+        Add a double in protocol encoder buffer
+        """
         self.__encoder.writeDouble(v)
         return
     
     def writeString(self, v):
+        """
+        Add a string in protocol encoder buffer
+        """
         self.__encoder.writeString(v)
         return
     
     def writeSymbol(self, v):
+        """
+        Add a symbol in protocol encoder buffer
+        """
         self.__encoder.writeSymbol(v)
         return
     
     def writeRawString(self, v):
+        """
+        Add a rawstring in protocol encoder buffer
+        """
         self.__encoder.writeRawString(v)
         return
     
     def writeObject(self, arg): # Could be a FtsObject or an int
+        """
+        Add an object in protocol encoder buffer
+        """
         self.__encoder.writeObject(arg)
         return
     
-    def writeAtoms(self, atoms):        # tuple of atoms
-        self.__encoder.writeAtoms(atoms)
-        return
-    
     def writeArgs(self, v):
+        """
+        Add a list of args in protocol encoder buffer
+        """
         self.__encoder.writeArgs(v)
         return
     
     def endOfMessage(self):
+        """
+        Add END_Of_MESSAGE tag in protocol encoder buffer and send
+        message to FTS server
+        """
         self.__encoder.endOfMessage()
         self.write(self.__encoder.getBytes(), self.__encoder.getLength())
         self.__encoder.clear()
         return
     
     def read(self, byte, length):
+        """
+        Method to be implemented in derived classes
+        """
         raise NotImplementedError
     
     def write(self, byte, length):
+        """
+        Method to be implemented in derived classes
+        """
         raise NotImplementedError
     
     def close(self):
+        """
+        Method to be implemented in derived classes
+        """
         raise NotImplementedError
     
     def receiveThread(self):
+        """
+        Received thread
+        """
         byte = []
         while self.runningThread == 1:
             try:
