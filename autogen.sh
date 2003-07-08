0f3c528cc667e3b7542f44cd0e1053d013ffb6c9
p_tisserand <p_tisserand>
2003-07-08 14:59:47 +0000

    Client configure need to know some macro defined in jmax_top_dir/m4 directory.

--- autogen.sh
+++ autogen.sh
@@ -4,11 +4,14 @@ set -x
 aclocal -I m4
 # On Mac OS X, libtoolize is glibtoolize
 libtoolize --force --automake --copy || glibtoolize --force --automake --copy
+#autoheader
 autoconf
 automake --foreign --add-missing --copy
 (
     cd client
-    aclocal
+    # client configure use m4 macro defined in jmax root m4 directory (jar -u support)
+    aclocal -I ../m4
+#    autoheader
     autoconf
     automake --foreign --add-missing --copy
 )
