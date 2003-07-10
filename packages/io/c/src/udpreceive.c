fc532949bb05408ea33e53ca0619c6bdccc71af1
p_tisserand <p_tisserand>
2003-07-10 10:26:37 +0000

    Changed udpsend/udpreceive protocol to jMax 2.5.x protocol.

--- packages/io/c/src/udpreceive.c
+++ packages/io/c/src/udpreceive.c
@@ -17,15 +17,153 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * Based on Max/ISPW by Miller Puckette.
+ *
  *
  */
 
+#include <unistd.h>
 #include <string.h>
-
+#include <ctype.h>
 #include <stdio.h>
 
 #include <fts/fts.h>
 
+#include "jmax25_protocol.h"
+
+/* ********************************************************************** */
+/* Protocol decoder                                                       */
+/* ********************************************************************** */
+
+enum decode_state { 
+  STATE_IN_TYPE,
+  STATE_IN_INT,
+  STATE_IN_FLOAT,
+  STATE_IN_STRING
+};
+
+#define STRING_MAX 256
+
+typedef struct _protodecode_t {
+  enum decode_state state;
+  int counter;
+  int int_value;
+  float float_value;
+  int string_len;
+  char string_value[STRING_MAX];
+} protodecode_t;
+
+typedef enum {
+  INT_TOKEN,
+  FLOAT_TOKEN,
+  STRING_TOKEN,
+  EOM_TOKEN,
+  RUNNING
+} protodecode_status_t;
+
+
+void protodecode_init( protodecode_t *pr)
+{
+  pr->state = STATE_IN_TYPE;
+}
+
+protodecode_status_t protodecode_run( protodecode_t *pr, unsigned char b)
+{
+  int ivalue;
+
+  switch( pr->state) {
+  case STATE_IN_TYPE:
+    if ( b == JMAX25_INT_CODE)
+      {
+	pr->state = STATE_IN_INT;
+	pr->counter = 4;
+	pr->int_value = 0;
+	return RUNNING;
+      }
+    else if ( b == JMAX25_FLOAT_CODE)
+      {
+	unsigned int zero = 0;
+
+	pr->state = STATE_IN_FLOAT;
+	pr->counter = 4;
+	pr->float_value = *((float *)&zero);
+	return RUNNING;
+      }
+    else if ( b == JMAX25_STRING_CODE)
+      {
+	pr->state = STATE_IN_STRING;
+	pr->string_len = 0;;
+	return RUNNING;
+      }
+    else if ( b == JMAX25_EOM_CODE)
+      return EOM_TOKEN;
+    break;
+
+  case STATE_IN_INT:
+    pr->int_value = (pr->int_value << 8) | b;
+
+    pr->counter--;
+    if (pr->counter == 0)
+      {
+	pr->state = STATE_IN_TYPE;
+	return INT_TOKEN;
+      }
+    else
+      return RUNNING;
+
+  case STATE_IN_FLOAT:
+    ivalue = *((unsigned int *)&(pr->float_value));
+    ivalue = (ivalue << 8) | b;
+    pr->float_value = *((float *)&ivalue);
+
+    pr->counter--;
+    if (pr->counter == 0)
+      {
+	pr->state = STATE_IN_TYPE;
+	return FLOAT_TOKEN;
+      }
+    else
+      return RUNNING;
+
+  case STATE_IN_STRING:
+    if ( b != JMAX25_STRING_END_CODE)
+      {
+	/* TODO: should realloc() on buffer overflow */
+	if ( pr->string_len < STRING_MAX)
+	  {
+	    pr->string_value[ pr->string_len] = (char)b;
+	    pr->string_len++;
+	  }
+	
+	return RUNNING;
+      }
+    else 
+      {
+	pr->string_value[ pr->string_len ] = '\0';
+	pr->state = STATE_IN_TYPE;
+
+	return STRING_TOKEN;
+      }
+  }
+
+  return RUNNING;
+}
+
+int protodecode_get_int( protodecode_t *pr)
+{
+  return pr->int_value;
+}
+
+float protodecode_get_float( protodecode_t *pr)
+{
+  return pr->float_value;
+}
+
+char *protodecode_get_string( protodecode_t *pr)
+{
+  return pr->string_value;
+}
 
 /* ********************************************************************** */
 /* udpreceive object                                                      */
@@ -37,7 +175,7 @@
 typedef struct {
   fts_object_t o;
   fts_bytestream_t* udp_stream;
-  fts_binary_protocol_t* binary_protocol;
+  char buffer[UDP_PACKET_SIZE];
 } udpreceive_t;
 
 #define MAXATOMS 1024
@@ -45,38 +183,77 @@ typedef struct {
 static void udpreceive_receive( fts_object_t *o, int size, const unsigned char* buffer)
 {
   udpreceive_t *self = (udpreceive_t *)o;
-  fts_symbol_t selector = NULL;
-  int argc;
-  fts_atom_t *argv;
-  fts_binary_protocol_t* binary_protocol = self->binary_protocol;
+  fts_atom_t argv[MAXATOMS];
+  fts_symbol_t selector = 0;
+  int i, argc, first_token;
+  protodecode_t pr;
+
+/*   size = recvfrom( self->socket, self->buffer, UDP_PACKET_SIZE, 0, NULL, NULL); */
 
   if ( size <= 0)
     return;
 
-  if (fts_binary_protocol_decode(binary_protocol, size, buffer) != size)
+  protodecode_init( &pr);
+  argc = 0;
+  first_token = 0;
+
+  for ( i = 0; i < size; i++)
   {
-    fts_log("[udpreceive] error in protocol decoding \n");
-    fts_object_error(o, "error in protocol decoding");
-    return;
-  }
-
-  argc = fts_stack_size( &binary_protocol->input_args);
-  argv = (fts_atom_t *)fts_stack_base( &binary_protocol->input_args);
-
-  if (fts_is_symbol(argv))
-  {
-    selector = fts_get_symbol(argv);
-    argc--;
-    argv++;
-
-    if (argc < 0)
-    {      
-      argc = 0;
-      argv = NULL;
+    protodecode_status_t t;
+    
+    t = protodecode_run( &pr, buffer[i]);
+    
+    if ( t == INT_TOKEN)
+    {
+      if (first_token == 0)
+	first_token = 1;
+      
+      fts_set_int( &(argv[argc]), protodecode_get_int( &pr));
+      argc++;
     }
+    else if ( t == FLOAT_TOKEN)
+    {
+      if (first_token == 0)
+	first_token = 1;
+      
+      fts_set_float( &(argv[argc]), protodecode_get_float( &pr));
+      argc++;
+    }
+    else if ( t == STRING_TOKEN)
+    {
+      if (first_token == 0)
+      {
+	selector = fts_new_symbol( protodecode_get_string( &pr));
+	/* 
+	   jMax 2.5.x use stype symbol as first selector, so we need to remove 
+	   int and float symbl before sending it 
+	*/
+	if ((selector == fts_s_int)
+	    || (selector == fts_s_float)
+	    || (selector == fts_s_list))
+	{
+	  selector = NULL;
+	}
+	
+	if (selector == fts_s_bang)
+	{
+	  selector = NULL;
+	  fts_set_void(&argv[argc]);
+	  argc++;
+	}
+	first_token = 1;
+      }
+      else
+      {
+	fts_set_symbol( &(argv[argc]), fts_new_symbol(protodecode_get_string( &pr)));
+	argc++;
+      }
+    }
+    else if ( t == EOM_TOKEN)
+      break;
   }
+  
   fts_outlet_send( (fts_object_t *)self, 0, selector, argc, argv);
-  fts_stack_clear(&binary_protocol->input_args);
 }
 
 
@@ -84,15 +261,6 @@ static void udpreceive_init(fts_object_t *o, int winlet, fts_symbol_t s, int ac,
 {
   udpreceive_t *self = (udpreceive_t *)o;
   fts_bytestream_t* udp_stream;
-
-  /* create binary protocol decoder */
-  self->binary_protocol = (fts_binary_protocol_t*)fts_object_create(fts_binary_protocol_type, 0, NULL);
-  if (self->binary_protocol == NULL)
-  {
-    fts_object_error(o, "Cannot create binary protocol component");
-    return;
-  }
-  fts_object_refer((fts_object_t*)self->binary_protocol);
 
   /* check number of argument */
   if ((ac == 1) 
@@ -125,15 +293,13 @@ static void udpreceive_init(fts_object_t *o, int winlet, fts_symbol_t s, int ac,
   fts_object_set_outlets_number(o, 1);
 
   fts_bytestream_add_listener(self->udp_stream, (fts_object_t*)self, udpreceive_receive);
+
 }
 
 static void udpreceive_delete(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
   udpreceive_t *self = (udpreceive_t *)o;
-  if (self->binary_protocol != NULL)
-  {
-    fts_object_release((fts_object_t*)self->binary_protocol);
-  }
+
   if (self->udp_stream != NULL)
   {
     fts_bytestream_remove_listener(self->udp_stream, o);
@@ -143,7 +309,7 @@ static void udpreceive_delete(fts_object_t *o, int winlet, fts_symbol_t s, int a
 
 static void udpreceive_instantiate(fts_class_t *cl)
 {
-  fts_class_init(cl, sizeof( udpreceive_t), udpreceive_init, udpreceive_delete);
+  fts_class_init(cl, sizeof(udpreceive_t), udpreceive_init, udpreceive_delete);
 }
 
 void udpreceive_config( void)
@@ -151,11 +317,9 @@ void udpreceive_config( void)
   fts_class_install( fts_new_symbol("udpreceive"), udpreceive_instantiate);
 }
 
-
 /** EMACS **
  * Local variables:
  * mode: c
  * c-basic-offset:2
  * End:
  */
-
--- packages/io/c/src/udpsend.c
+++ packages/io/c/src/udpsend.c
@@ -20,12 +20,164 @@
  * 
  */
 
+#include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <ctype.h>
 #include <stdio.h>
 
 #include <fts/fts.h>
 
+#include "jmax25_protocol.h"
 
+/* ********************************************************************** */
+/* Protocol encoder                                                       */
+/* ********************************************************************** */
+
+typedef struct _protoencode_t {
+  unsigned char *buffer;
+  int buffer_size;
+  int current;
+} protoencode_t;
+
+#define DEFAULT_BUFFER_SIZE 1024
+
+void protoencode_init( protoencode_t *pr)
+{
+  pr->buffer_size = DEFAULT_BUFFER_SIZE;
+
+  pr->buffer = (unsigned char *)fts_malloc( pr->buffer_size);
+
+  pr->current = 0;
+}
+
+void protoencode_destroy( protoencode_t *pr)
+{
+  fts_free( pr->buffer);
+  pr->buffer = 0;
+  pr->current = 0;
+  pr->buffer_size = 0;
+}
+
+unsigned char *protoencode_get_mess( protoencode_t *pr)
+{
+  return pr->buffer;
+}
+
+int protoencode_get_size( protoencode_t *pr)
+{
+  return pr->current;
+}
+
+void protoencode_start( protoencode_t *pr)
+{
+  pr->current = 0;
+}
+
+static void protoencode_realloc_buffer( protoencode_t *pr)
+{
+  int newsize = 2 * pr->buffer_size;
+
+  pr->buffer = (unsigned char *)fts_realloc( pr->buffer, pr->buffer_size);
+
+  pr->buffer_size = newsize;
+}
+
+void protoencode_end( protoencode_t *pr)
+{
+  if ( pr->current + 1 >= pr->buffer_size)
+    protoencode_realloc_buffer( pr);
+
+  pr->buffer[ pr->current ] = JMAX25_EOM_CODE;
+  pr->current++;
+}
+
+void protoencode_put_int( protoencode_t *pr, int value)
+{
+  unsigned char *p;
+
+  if (pr->current + (1 + 4) >= pr->buffer_size)
+    protoencode_realloc_buffer( pr);
+
+  p = pr->buffer + pr->current;
+
+  *p++ = JMAX25_INT_CODE;
+  *p++ = (unsigned char) (((unsigned int) value >> 24) & 0xff);
+  *p++ = (unsigned char) (((unsigned int) value >> 16) & 0xff);
+  *p++ = (unsigned char) (((unsigned int) value >> 8) & 0xff);
+  *p++ = (unsigned char) (((unsigned int) value >> 0) & 0xff);
+
+  pr->current += 5;
+}
+
+void protoencode_put_float( protoencode_t *pr, float value)
+{
+  unsigned char *p;
+  unsigned int ivalue;
+
+  if (pr->current + (1 + 4) >= pr->buffer_size)
+    protoencode_realloc_buffer( pr);
+
+  p = pr->buffer + pr->current;
+  ivalue = *((unsigned int *)&value);
+
+  *p++ = JMAX25_FLOAT_CODE;
+  *p++ = (unsigned char) ((ivalue >> 24) & 0xff);
+  *p++ = (unsigned char) ((ivalue >> 16) & 0xff);
+  *p++ = (unsigned char) ((ivalue >> 8) & 0xff);
+  *p++ = (unsigned char) ((ivalue >> 0) & 0xff);
+
+  pr->current += 5;
+}
+
+void protoencode_put_string( protoencode_t *pr, const char *s)
+{
+  int len = strlen( s);
+  unsigned char *p;
+
+  if (pr->current + (1 + len + 1) >= pr->buffer_size)
+    protoencode_realloc_buffer( pr);
+
+  p = pr->buffer + pr->current;
+
+  *p++ = JMAX25_STRING_CODE;
+
+  while (*s)
+    {
+      *p = *s;
+      p++;
+      s++;
+    }
+
+  *p++ = JMAX25_STRING_END_CODE;
+
+  pr->current += (2 + len);
+}
+
+void protoencode_put_tuple(protoencode_t *pr, fts_tuple_t* tup)
+{
+  int i;
+  int size = fts_tuple_get_size(tup);
+  fts_atom_t* at;
+  protoencode_put_string(pr, fts_s_list);
+  for (i = 0; i < size; i++)
+  {
+    at = fts_tuple_get_element(tup, i);
+    if (fts_is_int(at))
+    {
+      protoencode_put_int(pr, fts_get_int(at));
+    }
+    if (fts_is_float(at))
+    {
+      protoencode_put_float(pr, fts_get_float(at));
+    }
+    if (fts_is_symbol(at))
+    {
+      protoencode_put_string(pr, fts_get_symbol(at));
+    }
+  }  
+
+}
 /* ********************************************************************** */
 /* udpsend object                                                         */
 /* ********************************************************************** */
@@ -34,43 +186,13 @@
 typedef struct {
   fts_object_t o;
   fts_bytestream_t* udp_stream;
-  fts_binary_protocol_t* binary_protocol;
+  protoencode_t encoder;
 } udpsend_t;
-
-
-static void
-udpsend_input(fts_object_t* o, int winlet, fts_symbol_t s, int ac, const fts_atom_t* at)
-{
-  udpsend_t* self = (udpsend_t*)o;
-  fts_binary_protocol_t* binary_protocol = self->binary_protocol;
-
-  if (s != NULL)
-  {
-    /* add selector to message */
-    fts_binary_protocol_add_symbol(binary_protocol, s);
-  }
-  fts_binary_protocol_add_atoms(binary_protocol, ac, at);
-  fts_binary_protocol_end_message(binary_protocol);
-
-  fts_bytestream_output(self->udp_stream, 
-			fts_stack_size(&binary_protocol->output_buffer), 
-			fts_stack_base(&binary_protocol->output_buffer));
-  fts_stack_clear(&binary_protocol->output_buffer);
-}
 
 static void udpsend_init(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
   udpsend_t *self = (udpsend_t *)o;
   fts_bytestream_t* udp_stream;
-
-  /* create binary protocol */
-  self->binary_protocol = (fts_binary_protocol_t*)fts_object_create(fts_binary_protocol_type, 0, NULL);
-  if (self->binary_protocol == NULL)
-  {
-    fts_object_error(o, "Cannot create binary protocol component");
-    return;
-  }
-  fts_object_refer((fts_object_t*)self->binary_protocol);
 
   /* check number of argument */
   if ((ac == 1) 
@@ -99,27 +221,85 @@ static void udpsend_init(fts_object_t *o, int winlet, fts_symbol_t s, int ac, co
 
   self->udp_stream = udp_stream;
   fts_object_refer((fts_object_t*)self->udp_stream);
+
+  protoencode_init( &self->encoder);
 }
 
 static void udpsend_delete(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
   udpsend_t *self = (udpsend_t *)o;
-  if (self->binary_protocol != NULL)
-  {
-    fts_object_release((fts_object_t*)self->binary_protocol);
-  }
+
   if (self->udp_stream != NULL)
   {
     fts_object_release((fts_object_t*)self->udp_stream);
   }
+
+  protoencode_destroy( &self->encoder);
 }
 
-static void 
+static void udpsend_anything(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
+{
+  int i, size, r;
+  udpsend_t *self = (udpsend_t *)o;
+  char *buff;
+  fts_symbol_t selector = s;
+  protoencode_start( &self->encoder);
+
+  if (NULL != selector)
+  {
+    protoencode_put_string( &self->encoder, selector);
+  }
+  else if (0 == ac)
+  {
+    /* this is a bang */
+    protoencode_put_string(&self->encoder, fts_s_bang);
+  }
+
+  while (ac--)
+  {
+    if (fts_is_int( at))
+    {
+      if (selector == NULL)
+      {
+	selector = fts_s_int;
+	protoencode_put_string(&self->encoder, selector);
+      }
+      protoencode_put_int( &self->encoder, fts_get_int( at));
+    }
+    else if (fts_is_float( at))
+    {
+      if (selector == NULL)
+      {
+	selector = fts_s_float;
+	protoencode_put_string(&self->encoder, selector);
+      }
+      protoencode_put_float( &self->encoder, fts_get_float( at));
+    }
+    else if (fts_is_symbol( at))
+    {
+      protoencode_put_string( &self->encoder, fts_get_symbol( at));
+    }
+    if (fts_is_tuple(at))
+    {
+      protoencode_put_tuple(&self->encoder, (fts_tuple_t*)fts_get_object(at));
+    }
+    at++;
+  }
+  
+  protoencode_end( &self->encoder);
+  
+  buff = protoencode_get_mess( &self->encoder);
+  size = protoencode_get_size( &self->encoder);
+  
+  fts_bytestream_output(self->udp_stream, size, buff);
+}
+
+static void
 udpsend_instantiate(fts_class_t *cl)
 {
-  fts_class_init(cl, sizeof( udpsend_t), udpsend_init, udpsend_delete);
+  fts_class_init( cl, sizeof( udpsend_t), udpsend_init, udpsend_delete);
 
-  fts_class_input_handler(cl, udpsend_input);
+  fts_class_input_handler(cl, udpsend_anything);
 }
 
 void udpsend_config( void)
@@ -133,4 +313,3 @@ void udpsend_config( void)
  * c-basic-offset:2
  * End:
  */
-
