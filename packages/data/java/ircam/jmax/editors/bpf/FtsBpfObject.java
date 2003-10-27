2aabb2c6a49a6edd15237484c71acbac0cc25ff7
p_tisserand <p_tisserand>
2003-10-27 11:54:37 +0000

    Fixed bug "[ 827862 ] bpf persistence buggy". Added a check on nArgs in append method to prevent ArrayIndexOutOfBoundsException throwing.

--- packages/data/java/ircam/jmax/editors/bpf/FtsBpfObject.java
+++ packages/data/java/ircam/jmax/editors/bpf/FtsBpfObject.java
@@ -173,11 +173,13 @@ public class FtsBpfObject extends FtsObjectWithEditor implements BpfDataModel
 
   public void append(int nArgs , FtsAtom args[])
   {
-    int j=0;
-    for(int i = length(); i<length()+nArgs; i+=2)
-      addPoint(j++, new BpfPoint( (float)args[i].doubleValue, (float)args[i+1].doubleValue));
-	
-    notifyPointAdded(j);
+    int j = length();
+    if (nArgs > 1)
+    {
+      for(int i = 0; i< nArgs; i+=2)
+	addPoint(j++, new BpfPoint( (float)args[i].doubleValue, (float)args[i+1].doubleValue));
+      notifyPointAdded(j-1);
+    }
   }
     
   /*
