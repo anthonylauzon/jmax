698585a5a54b1acdf0837ca1c0578b8f3f07d080
p_tisserand <p_tisserand>
2003-10-31 18:32:34 +0000

    Fixed bug ""[ 832367 ] [change] does not register a difference". Now toggle implements an input handler. You can change the state of toggle with sending a set message to the 2nd inlet. The new state is the set message argument. If set is send to the 1st inlet, the message is understood as a new value (so it explains why toggle as 2 inlets..).

--- packages/control/c/src/change.c
+++ packages/control/c/src/change.c
@@ -29,71 +29,139 @@ typedef struct
   fts_atom_t state;
 } change_t;
 
-static void
-change_int(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
-{
-  change_t *this = (change_t *)o;
-
-  if(!fts_is_number(&this->state) || fts_get_number_int(&this->state) != fts_get_int(at))
-    {
-      fts_atom_release(&this->state);
-      this->state = at[0];
-      fts_outlet_atom(o, 0, at);
-    }
-}
-
-static void
-change_float(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
-{
-  change_t *this = (change_t *)o;
-
-  if(!fts_is_number(&this->state) || fts_get_number_float(&this->state) != fts_get_float(at))
-    {
-      fts_atom_release(&this->state);
-      this->state = at[0];
-      fts_outlet_atom(o, 0, at);
-    }
-}
-
-static void
-change_symbol(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
-{
-  change_t *this = (change_t *)o;
-
-  if(!fts_is_symbol(&this->state) || fts_get_symbol(&this->state) != fts_get_symbol(at))
-    {
-      fts_atom_release(&this->state);
-      this->state = at[0];
-      fts_outlet_atom(o, 0, at);
-    }
-}
-
-static void
-change_input(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
-{
-  change_t *this = (change_t *)o;
-
-  if(!fts_atom_equals(&this->state, at))
-    {
-      fts_atom_assign(&this->state, at);
-      fts_outlet_atom(o, 0, at);
-    }
-}
 
 static void
 change_set(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
-  change_t *this = (change_t *)o;
+  change_t *self = (change_t *)o;
 
-  fts_atom_assign(&this->state, at);
+  if (ac > 0)
+  {
+    fts_atom_assign(&self->state, at);
+  }
+  else
+  {
+    /* we set state at void */
+    if (!fts_is_void(&self->state))
+    {
+      fts_atom_void(&self->state);
+    }
+  }
+}
+
+
+static void 
+change_anything(fts_object_t* o, int winlet, fts_symbol_t s, int ac, const fts_atom_t* at)
+{
+  change_t* self = (change_t*)o;
+  fts_tuple_t* atup;
+  fts_atom_t a;
+  
+  /* special case if we receive symbol set at inlet 1*/
+  if ((1 == winlet)
+      && (fts_s_set == s))
+  {
+    change_set(o, winlet, s, ac, at);
+  }
+  else
+  {
+    if (NULL == s)
+    {
+      /* no selector */
+      switch (ac)
+      {
+      case 0:
+	if (!fts_is_void(&self->state))
+	{
+	  fts_atom_void(&self->state);
+	  fts_outlet_atom(o, 0, &self->state);
+	}
+	break;
+      case 1:
+	if (!fts_atom_same_type(&self->state, at))
+	{
+	  fts_atom_assign(&self->state, at);
+	  fts_outlet_atom(o, 0, at);
+	}
+	else
+	{
+	  if (!fts_is_object(at))
+	  {
+	    if (!fts_atom_identical(&self->state, at))
+	    {
+	      fts_atom_assign(&self->state, at);
+	      fts_outlet_atom(o, 0, at);
+	    }
+	  }
+	  else
+	  {
+	    fts_object_t* obj1 = fts_get_object(&self->state);
+	    fts_object_t* obj2 = fts_get_object(at);
+	    if (obj1 != obj2)
+	    {
+	      fts_class_t* class = fts_object_get_class(obj1);
+	      fts_equals_function_t equals = fts_class_get_equals_function(class);
+	      if ((NULL == equals)
+		  || (0 == (*equals)(&self->state, at)))
+	      {
+		fts_atom_assign(&self->state, at);
+		fts_outlet_atom(o, 0, at);
+	      }
+	    }
+	  }
+	}
+	break;
+      default:
+	atup = (fts_tuple_t*)fts_object_create(fts_tuple_class, ac, at);
+	fts_object_refer(atup);
+	fts_set_object(&a, atup);
+	if (!fts_atom_equals(&self->state, &a))
+	{
+	  fts_atom_assign(&self->state, &a);
+	  fts_outlet_atom(o, 0, &a);
+	}
+	fts_object_release(atup);
+	break;
+      }
+    }
+    else
+    {
+      if (ac > 0)
+      {
+	/* create a tuple with selector and arguments ... */
+	atup = (fts_tuple_t*)fts_object_create(fts_tuple_class, ac, at);
+	fts_object_refer(atup);
+	fts_tuple_prepend_symbol(atup, s);
+	fts_set_object(&a, atup);
+	if (!fts_atom_equals(&self->state, &a))
+	{
+	  fts_atom_assign(&self->state, &a);
+	  fts_outlet_atom(o, 0, &a);
+	}
+	fts_object_release(atup);
+      }
+      else
+      {
+	/* we use the selector as current state */
+	if (s != fts_get_symbol(&self->state))
+	{
+	  fts_atom_t a;
+	  fts_set_symbol(&a, s);
+	  fts_atom_assign(&self->state, &a);
+	  fts_outlet_atom(o, 0, &a);
+	}
+      }
+    }
+  }
+
 }
 
 static void
 change_init(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
-  change_t *this = (change_t *)o;
+  change_t *self = (change_t *)o;
 
-  fts_set_void(&this->state);
+  fts_set_void(&self->state);
 
   if(ac > 0)
     fts_send_message(o, fts_s_set, ac, at);
@@ -102,9 +170,9 @@ change_init(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_
 static void
 change_delete(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
-  change_t *this = (change_t *)o;
+  change_t *self = (change_t *)o;
 
-  fts_atom_void(&this->state);
+  fts_atom_void(&self->state);
 }
 
 static void
@@ -112,13 +180,7 @@ change_instantiate(fts_class_t *cl)
 {
   fts_class_init(cl, sizeof(change_t), change_init, change_delete);
 
-  fts_class_message_varargs(cl, fts_s_set, change_set);
-  
-  fts_class_inlet_int(cl, 0, change_int);
-  fts_class_inlet_float(cl, 0, change_float);
-  fts_class_inlet_symbol(cl, 0, change_symbol);
-  fts_class_inlet_atom(cl, 0, change_input);
-
+  fts_class_input_handler(cl, change_anything);
   fts_class_inlet_atom(cl, 1, change_set);
 
   fts_class_outlet_atom(cl, 0);
@@ -130,3 +192,9 @@ change_config(void)
   fts_class_install(fts_new_symbol("change"), change_instantiate);
 }
 
+/** EMACS **
+ * Local variables:
+ * mode: c
+ * c-basic-offset:2
+ * End:
+ */
