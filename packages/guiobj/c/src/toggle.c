645723e18fa1de8496b634a47c963005118c6517
p_tisserand <p_tisserand>
2003-10-31 15:08:30 +0000

    Fixed bug "[ 832351 ] persistence on toggle does not work". toggle_set method is now called when receiving fts_s_value message. Added persistence support. Added name support. Added handler for fts_s_dump and fts_s_dump_state.

--- packages/guiobj/c/src/toggle.c
+++ packages/guiobj/c/src/toggle.c
@@ -26,57 +26,58 @@ typedef struct
 {
   fts_object_t o;
   int value;
+  int persistence;
 } toggle_t;
 
 
 static void 
 toggle_update_real_time(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
-  toggle_t *this = (toggle_t *) o;
+  toggle_t *self = (toggle_t *) o;
   fts_atom_t a;
 
-  fts_set_int( &a, this->value);  
+  fts_set_int( &a, self->value);  
   fts_client_send_message_real_time(o, fts_s_value, 1, &a);
 }
 
 static void 
 toggle_set(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
-  toggle_t *this = (toggle_t *) o;
+  toggle_t *self = (toggle_t *) o;
 
   if(ac > 0 && fts_is_number(at))
+  {
+    int n = (fts_get_number_int(at) != 0);
+    
+    if(self->value != n)
     {
-      int n = (fts_get_number_int(at) != 0);
-
-      if(this->value != n)
-	{
-	  this->value = n;
-	  
-	  fts_update_request(o);
-	}
+      self->value = n;
+      
+      fts_update_request(o);
     }
+  }
 }
 
 static void 
 toggle_number(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
-  toggle_t *this = (toggle_t *) o;
+  toggle_t *self = (toggle_t *) o;
 
   toggle_set(o, 0, 0, 1, at);
 
-  fts_outlet_int(o, 0, this->value);
+  fts_outlet_int(o, 0, self->value);
 }
 
 static void 
 toggle_toggle(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const fts_atom_t *at)
 {
-  toggle_t *this = (toggle_t *) o;
+  toggle_t *self = (toggle_t *) o;
 
-  this->value = !this->value;
+  self->value = !self->value;
 
   fts_update_request(o);
 
-  fts_outlet_int(o, 0, this->value);
+  fts_outlet_int(o, 0, self->value);
 }
 
 static void 
@@ -94,15 +95,101 @@ toggle_save_dotpat(fts_object_t *o, int winlet, fts_symbol_t s, int ac, const ft
   fprintf( file, "#P toggle %d %d %d;\n", fts_get_int( &xa), fts_get_int( &ya), fts_get_int( &wa) );
 }
 
+static void
+toggle_dump_state(fts_object_t* o, int winlet, fts_symbol_t s, int ac, const fts_atom_t* at)
+{
+  toggle_t* self = (toggle_t*)o;
+  fts_dumper_t* dumper = (fts_dumper_t*)fts_get_object(at);
+  fts_atom_t a;
+  
+  fts_set_int(&a, self->value);
+  fts_dumper_send(dumper, fts_s_value, 1, &a);
+}
+
+static void
+toggle_dump(fts_object_t* o, int winlet, fts_symbol_t s, int ac, const fts_atom_t* at)
+{
+  toggle_t* self = (toggle_t*)o;
+  fts_dumper_t* dumper = (fts_dumper_t*)fts_get_object(at);
+  fts_atom_t a;
+
+  /* save name if any */
+  fts_name_dump_method(o, 0, 0, ac, at);
+  if (self->persistence != 0)
+  {
+    toggle_dump_state(o, 0, 0, ac, at);
+    
+    fts_set_int(&a, 1);
+    fts_dumper_send(dumper, fts_s_persistence, 1, &a);
+  }
+}
+
+static void
+toggle_persistence(fts_object_t* o, int winlet, fts_symbol_t s, int ac, const fts_atom_t* at)
+{
+  toggle_t* self = (toggle_t*)o;
+  
+  if (ac > 0)
+  {
+    /* set persistence flag */
+    if (fts_is_number(at))
+    {
+      self->persistence = (fts_get_number_int(at) != 0);
+      fts_client_send_message(o, fts_s_persistence, 1, at);
+    }
+  }
+  else
+  {
+    fts_atom_t a;
+    fts_set_int(&a, self->persistence);
+    fts_return(&a);
+  }
+}
+
+
+static void
+toggle_update_gui(fts_object_t* o, int winlet, fts_symbol_t s, int ac, const fts_atom_t* at)
+{
+  toggle_t* self = (toggle_t*)o;
+  fts_atom_t a;
+  
+  fts_set_int(&a, self->persistence);
+  fts_client_send_message(o, fts_s_persistence, 1, &a);
+
+  fts_set_int( &a, self->value);  
+  fts_client_send_message_real_time(o, fts_s_value, 1, &a);
+  
+  fts_name_gui_method(o, 0, 0, 0, 0);
+}
+
+static void
+toggle_init(fts_object_t* o, int winlet, fts_symbol_t s, int ac, const fts_atom_t* at)
+{
+  toggle_t* self = (toggle_t*)o;
+  
+  self->persistence = 0;
+  self->value = 0;
+}
+
 static void 
 toggle_instantiate(fts_class_t *cl)
 {
-  fts_class_init(cl, sizeof(toggle_t), 0, 0);
+  fts_class_init(cl, sizeof(toggle_t), toggle_init, 0);
 
   fts_class_message_varargs(cl, fts_s_update_real_time, toggle_update_real_time); 
   fts_class_message_varargs(cl, fts_s_save_dotpat, toggle_save_dotpat); 
 
+  /* persistence part */
+  fts_class_message_varargs(cl, fts_s_persistence, toggle_persistence);
+  fts_class_message_varargs(cl, fts_s_dump_state, toggle_dump_state);
+  fts_class_message_varargs(cl, fts_s_dump, toggle_dump);
+
+  /* name support */
+  fts_class_message_varargs(cl, fts_s_name, fts_name_set_method);
+  fts_class_message_varargs(cl, fts_s_update_gui, toggle_update_gui);
+  
   fts_class_message_varargs(cl, fts_s_set, toggle_toggle);
+  fts_class_message_varargs(cl, fts_s_value, toggle_set);
 
   fts_class_message_varargs(cl, fts_new_symbol("click"), toggle_toggle);
 
@@ -118,3 +205,10 @@ toggle_config(void)
 {
   fts_class_install(fts_new_symbol("toggle"),toggle_instantiate);
 }
+
+/** EMACS **
+ * Local variables:
+ * mode: c
+ * c-basic-offset:2
+ * End:
+ */
