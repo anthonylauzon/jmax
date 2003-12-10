79947620a5b46c0ad2a4be1415317394aa9774b0
p_tisserand <p_tisserand>
2003-12-10 18:02:33 +0000

    Fixed bug "[ 840782 ] can't remove object name". If fts_object_set_name is called with fts_s_empty_string as argument, now we remove the object name.

--- fts/object.c
+++ fts/object.c
@@ -138,12 +138,17 @@ fts_object_set_name(fts_object_t *obj, fts_symbol_t sym)
         fts_client_send_message(obj, fts_s_name, 1, &a);
       }
     }
-    else if(fts_object_has_id(obj))
+    else 
     {
-      fts_atom_t a;
-
-      fts_set_symbol(&a, fts_s_empty_string);
-      fts_client_send_message(obj, fts_s_name, 1, &a);
+	/* we cleared the name since we have an empty string */
+	fts_object_remove_name(obj);
+	if(fts_object_has_id(obj))
+	{
+	    fts_atom_t a;
+	    
+	    fts_set_symbol(&a, fts_s_empty_string);
+	    fts_client_send_message(obj, fts_s_name, 1, &a);
+	}
     }
   }
 }
