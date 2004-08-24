5a3a62bdaac8a380d9cfacb06c2d8d08b0b6c880
diemo <diemo>
2004-08-24 10:41:02 +0000

    print versions, remove all caches

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
-rm aclocal.m4 config.* install-sh libtool ltconfig ltmain.sh missing mkinstalldirs
+
+echo "[auto-tools versions in your path on $HOST]"
+aclocal    --version | head -1
+automake   --version | head -1
+autom4te   --version | head -1
+autoconf   --version | head -1
+libtoolize --version | head -1
+libtool    --version | head -1
+
+rm -rf aclocal.m4 config.* install-sh libtool ltconfig ltmain.sh missing mkinstalldirs autom4te.cache
+
 set -x
 aclocal -I m4
 # On Mac OS X, libtoolize is glibtoolize
 libtoolize --force --automake --copy || glibtoolize --force --automake --copy
-#autoheader
+# autoheader
 autoconf
 automake --foreign --add-missing --copy
+
 (
     cd client
-    # client configure use m4 macro defined in jmax root m4 directory (jar -u support)
+
+    rm -rf aclocal.m4 config.* install-sh libtool ltconfig ltmain.sh missing mkinstalldirs autom4te.cache
+
+    # client configure uses m4 macros defined in jmax root m4 directory (jar -u support)
     aclocal -I ../m4
-#    autoheader
+#   autoheader
     autoconf
     automake --foreign --add-missing --copy
 )
