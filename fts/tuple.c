7643a58a75b19690b8912146c0769152bad62723
nschnell <nschnell>
2004-09-03 07:08:39 +0000

    Oops!

--- fts/tuple.c
+++ fts/tuple.c
@@ -2,7 +2,7 @@
  * jMax
  * Copyright (C) 1994, 1995, 1998, 1999 by IRCAM-Centre Georges Pompidou, Paris, France.
  * 
- * self program is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2.1
  * of the License, or (at your option) any later version.
